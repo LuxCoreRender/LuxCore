bb7e35fe9c48aeacb6c54a1380051134c2fff903
David Bucciarelli <dade@ngi.it>
2010-02-17 15:26:33 +0100

    Reviewed DeviceDescription filtering

--- src/core/device.cpp
+++ src/core/device.cpp
@@ -31,6 +31,65 @@
 
 namespace luxrays {
 
+//------------------------------------------------------------------------------
+// DeviceDescription
+//------------------------------------------------------------------------------
+
+void DeviceDescription::Filter(std::vector<DeviceDescription *> &deviceDescriptions) {
+	int gpuIndex = -1;
+	int cpuIndex = -1;
+	for (size_t i = 0; i < deviceDescriptions.size(); ++i) {
+		if ((cpuIndex == -1) && (deviceDescriptions[i]->GetType() == DEVICE_TYPE_NATIVE_THREAD))
+			cpuIndex = i;
+		else if ((gpuIndex == -1) && (deviceDescriptions[i]->GetType() == DEVICE_TYPE_OPENCL)) {
+			gpuIndex = i;
+			break;
+		}
+	}
+
+	if (gpuIndex != -1) {
+		std::vector<DeviceDescription *> selectedDev;
+		selectedDev.push_back(deviceDescriptions[gpuIndex]);
+		deviceDescriptions = selectedDev;
+	} else if (gpuIndex != -1) {
+		std::vector<DeviceDescription *> selectedDev;
+		selectedDev.push_back(deviceDescriptions[cpuIndex]);
+		deviceDescriptions = selectedDev;
+	} else
+		deviceDescriptions.clear();
+}
+
+void DeviceDescription::Filter(const DeviceType type,
+		std::vector<DeviceDescription *> &deviceDescriptions) {
+	size_t i = 0;
+	while (i < deviceDescriptions.size()) {
+		if ((type != DEVICE_TYPE_ALL) && (deviceDescriptions[i]->GetType() != type)) {
+			// Remove the device from the list
+			deviceDescriptions.erase(deviceDescriptions.begin() + i);
+		} else
+			++i;
+	}
+}
+
+std::string DeviceDescription::GetDeviceType(const DeviceType type) {
+	switch (type) {
+		case DEVICE_TYPE_ALL:
+			return "ALL";
+		case DEVICE_TYPE_NATIVE_THREAD:
+			return "NATIVE_THREAD";
+		case DEVICE_TYPE_OPENCL:
+			return "OPENCL";
+		case DEVICE_TYPE_VIRTUAL:
+			return "VIRTUAL";
+		default:
+			return "UNKNOWN";
+	}
+}
+
+//------------------------------------------------------------------------------
+// IntersectionDevice
+//------------------------------------------------------------------------------
+
 IntersectionDevice::IntersectionDevice(const Context *context, const DeviceType type, const unsigned int index) :
 	deviceContext(context), deviceType(type) {
 	deviceIndex = index;
@@ -60,33 +119,6 @@ void IntersectionDevice::Start() {
 void IntersectionDevice::Stop() {
 	assert (started);
 	started = false;
-}
-
-void IntersectionDevice::Filter(const DeviceType type,
-		std::vector<DeviceDescription *> &deviceDescriptions) {
-	size_t i = 0;
-	while (i < deviceDescriptions.size()) {
-		if ((type != DEVICE_TYPE_ALL) && (deviceDescriptions[i]->type != type)) {
-			// Remove the device from the list
-			deviceDescriptions.erase(deviceDescriptions.begin() + i);
-		} else
-			++i;
-	}
-}
-
-std::string IntersectionDevice::GetDeviceType(const DeviceType type) {
-	switch (type) {
-		case DEVICE_TYPE_ALL:
-			return "ALL";
-		case DEVICE_TYPE_NATIVE_THREAD:
-			return "NATIVE_THREAD";
-		case DEVICE_TYPE_OPENCL:
-			return "OPENCL";
-		case DEVICE_TYPE_VIRTUAL:
-			return "VIRTUAL";
-		default:
-			return "UNKNOWN";
-	}
 }
 
 //------------------------------------------------------------------------------
@@ -556,8 +588,8 @@ void OpenCLIntersectionDevice::Filter(const OpenCLDeviceType type,
 
 	size_t i = 0;
 	while (i < deviceDescriptions.size()) {
-		if ((deviceDescriptions[i]->type == DEVICE_TYPE_OPENCL) &&
-				(((OpenCLDeviceDescription *)deviceDescriptions[i])->oclType != type)) {
+		if ((deviceDescriptions[i]->GetType() == DEVICE_TYPE_OPENCL) &&
+				(((OpenCLDeviceDescription *)deviceDescriptions[i])->GetOpenCLType() != type)) {
 			// Remove the device from the list
 			deviceDescriptions.erase(deviceDescriptions.begin() + i);
 		} else
