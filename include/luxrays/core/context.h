848c5d6cd13fa03103eb60dc2892717624d7fa73
David Bucciarelli <dade@ngi.it>
2010-08-21 14:09:39 +0200

    Working LuxRays Doxygen documentation

--- include/luxrays/core/context.h
+++ include/luxrays/core/context.h
@@ -19,6 +19,21 @@
  *   LuxRays website: http://www.luxrender.net                             *
  ***************************************************************************/
 
+/*! \file
+ *
+ *   \brief LuxRays is a library dedicated to accelerate the ray intersection process by using GPUs.
+ *   \author BUCCIARELLI David, DUCHARME Alain, VERWIEBE Jens, BECH Tom et al.
+ *   \version 1.0
+ *   \date March 2010
+ *
+ */
+
+/*! \mainpage
+ * \section intro Introduction
+ * LuxRays is a library dedicated to accelerate the ray intersection process by
+ * using GPUs.
+ */
+
 #ifndef _LUXRAYS_CONTEXT_H
 #define	_LUXRAYS_CONTEXT_H
 
@@ -39,31 +54,112 @@ class DeviceDescription;
 class OpenCLDeviceDescription;
 class OpenCLIntersectionDevice;
 
+/*! \brief Interface to all main LuxRays functions.
+ *
+ * A Context is the main tool to access all LuxRays functionalities. It includes
+ * methods to list and create devices, to define the data set to use and to
+ * start/stop all the activities.
+ */
 class Context {
 public:
+	/*! \brief Construct a new LuxRays Context for the optionally defined OpenCL platform.
+	 *
+	 * \param handler is an optional pointer to a debug message handler. I can be NULL.
+	 * \param openclPlatformIndex is the index of the OpenCL platform to use (the
+	 *	order is the one returned by cl::Platform::get() function). If the values is -1,
+	 *	the first available platform is selected.
+	 */
 	Context(LuxRaysDebugHandler handler = NULL, const int openclPlatformIndex = -1);
+
+	/*!	\brief Free all Context associated resources.
+	 */
 	~Context();
 
+	//--------------------------------------------------------------------------
+	// Methods dedicated to device listing and creation
+	//--------------------------------------------------------------------------
+
+	/*!	\brief Return a list of all aviable device descriptions within the Context.
+	 *
+	 *	\return the vector of all DeviceDescription available.
+	 */
 	const std::vector<DeviceDescription *> &GetAvailableDeviceDescriptions() const;
+
+	/*!	\brief Return a list of all intersection device created within the Context.
+	 *
+	 *	\return the vector of all IntersectionDevice in the Context.
+	 */
 	const std::vector<IntersectionDevice *> &GetIntersectionDevices() const;
-	const std::vector<IntersectionDevice *> &GetIntersectionDevices(DeviceDescription type) const;
+
+	/*!	\brief Return a list of all pixel device created within the Context.
+	 *
+	 *	\return the vector of all PixelDevice in the Context.
+	 */
 	const std::vector<PixelDevice *> &GetPixelDevices() const;
 
-	std::vector<IntersectionDevice *> AddIntersectionDevices(std::vector<DeviceDescription *> &deviceDesc);
+	/*!	\brief Create an IntersectionDevice within the Context.
+	 *
+	 *	\param deviceDesc is a DeviceDescription vector of the devices to create
+	 *
+	 *	\return the vector of all IntersectionDevice created.
+	 */
+	std::vector<IntersectionDevice *> AddIntersectionDevices(std::vector<DeviceDescription *> &deviceDescs);
+
+	/*!	\brief Create an Virtual Many-To-Many IntersectionDevice within the Context.
+	 *
+	 *	Create an Virtual Many-To-Many IntersectionDevice. This kind of device is
+	 *	useful when you have multiple threads producing work for multiple GPUs. All
+	 *	the routing of the work to the least busy GPU is handled by LuxRays.
+	 *
+	 *	\param count is the number of virtual devices to create.
+	 *	\param deviceDescs is a DeviceDescription vector of the devices used by virtual devices.
+	 *
+	 *	\return the vector of all virtual IntersectionDevice created.
+	 */
 	std::vector<IntersectionDevice *> AddVirtualM2MIntersectionDevices(const unsigned int count,
 		std::vector<DeviceDescription *> &deviceDescs);
+
+	/*!	\brief Create an Virtual Many-To-One IntersectionDevice within the Context.
+	 *
+	 *	Create an Virtual Many-To-One IntersectionDevice. This kind of device is
+	 *	useful when you have multiple threads producing work for a single GPU. All
+	 *	the routing of the work forward to the GPU and backward is handled by LuxRays.
+	 *
+	 *	\param count is the number of virtual devices to create.
+	 *	\param deviceDescs is a DeviceDescription vector of the devices used by virtual devices.
+	 *
+	 *	\return the vector of all virtual IntersectionDevice created.
+	 */
 	std::vector<IntersectionDevice *> AddVirtualM2OIntersectionDevices(const unsigned int count,
 		std::vector<DeviceDescription *> &deviceDescs);
 
-	std::vector<PixelDevice *> AddPixelDevices(std::vector<DeviceDescription *> &deviceDesc);
+	/*!	\brief Create an PixelDevice within the Context.
+	 *
+	 *	\param deviceDesc is a DeviceDescription vector of the devices to create
+	 *
+	 *	\return the vector of all PixelDevice created.
+	 */
+	std::vector<PixelDevice *> AddPixelDevices(std::vector<DeviceDescription *> &deviceDescs);
+
+	//--------------------------------------------------------------------------
+	// Methods dedicated to DataSet definition
+	//--------------------------------------------------------------------------
 
 	void SetDataSet(DataSet *dataSet);
 	void UpdateDataSet();
+
+	//--------------------------------------------------------------------------
+	// Methods dedicated to Context mangement (i.e. start/stop, etc.)
+	//--------------------------------------------------------------------------
 
 	void Start();
 	void Interrupt();
 	void Stop();
 	bool IsRunning() const { return started; }
+
+	//--------------------------------------------------------------------------
+	// Methods dedicated to message debug handling
+	//--------------------------------------------------------------------------
 
 	bool HasDebugHandler() const { return debugHandler != NULL; }
 	void PrintDebugMsg(const char *msg) const {
