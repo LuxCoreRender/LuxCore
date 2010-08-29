450feb4fec9a1689d0127c9205d6f6e76873a2aa
David Bucciarelli <dade@ngi.it>
2010-08-29 11:22:37 +0200

    Changed SLG version to 1.7beta1

--- samples/smallluxgpu/slgcfg.h
+++ samples/smallluxgpu/slgcfg.h
@@ -25,6 +25,6 @@
 // The configured options and settings for SmallLuxGPU
 
 #define SLG_VERSION_MAJOR "1"
-#define SLG_VERSION_MINOR "7beta1dev"
+#define SLG_VERSION_MINOR "7beta1"
 
 #endif	/* _SLG_CFG_H */
