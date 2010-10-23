d7894e1c6507fafbe7550f63c0a3c9b3532edca3
David Bucciarelli <dade@ngi.it>
2010-10-23 17:33:22 +0200

    working on first LuxMark skeleton

---
+++ samples/luxmark/main.cpp
@@ -0,0 +1,32 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2010 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRays.                                         *
+ *                                                                         *
+ *   LuxRays is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   LuxRays is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   LuxRays website: http://www.luxrender.net                             *
+ ***************************************************************************/
+
+#include <cstdlib>
+
+#include "mainwindow.h"
+
+int main(int argc, char** argv) {
+	QApplication app(argc, argv);
+	MainWindow *win = new MainWindow();
+
+	win->show();
+	return app.exec();
+}
---
+++ samples/luxmark/mainwindow.cpp
@@ -0,0 +1,42 @@
+ /***************************************************************************
+ *   Copyright (C) 1998-2010 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRays.                                         *
+ *                                                                         *
+ *   LuxRays is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   LuxRays is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   LuxRays website: http://www.luxrender.net                             *
+ ***************************************************************************/
+
+#include "mainwindow.h"
+#include "aboutdialog.h"
+
+MainWindow::MainWindow(QWidget *parent, Qt::WindowFlags flags) : QMainWindow(parent, flags),
+		ui(new Ui::MainWindow) {
+	ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+	delete ui;
+}
+
+void MainWindow::exitApp() {
+	exit(0);
+}
+
+void MainWindow::showAbout() {
+	AboutDialog *dialog = new AboutDialog();
+
+	dialog->exec();
+}
