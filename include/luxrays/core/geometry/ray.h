e7930ddb01affca099a8bf4b0d10439f4d8f3a1f
Jens Verwiebe <Jens Verwiebe@localhost>
2010-11-17 18:26:18 +0100

    Fix Blender exporter for revisions > 33029 ( order-change )

---
+++ include/luxrays/core/geometry/ray.h
@@ -0,0 +1,79 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2009 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+#ifndef _LUXRAYS_RAY_H
+#define _LUXRAYS_RAY_H
+
+#include "luxrays/core/geometry/vector.h"
+#include "luxrays/core/geometry/point.h"
+
+namespace luxrays {
+
+extern float RAY_EPSILON;
+
+class  Ray {
+public:
+	// Ray Public Methods
+	Ray() : maxt(std::numeric_limits<float>::infinity()) {
+		mint = RAY_EPSILON;
+	}
+
+	Ray(const Point &origin, const Vector &direction)
+		: o(origin), d(direction), maxt(std::numeric_limits<float>::infinity()) {
+		mint = RAY_EPSILON;
+	}
+
+	Ray(const Point &origin, const Vector &direction,
+		float start, float end = std::numeric_limits<float>::infinity())
+		: o(origin), d(direction), mint(start), maxt(end) { }
+
+	Point operator()(float t) const { return o + d * t; }
+	void GetDirectionSigns(int signs[3]) const {
+		signs[0] = d.x < 0.f;
+		signs[1] = d.y < 0.f;
+		signs[2] = d.z < 0.f;
+	}
+
+	// Ray Public Data
+	Point o;
+	Vector d;
+	mutable float mint, maxt;
+};
+
+inline std::ostream &operator<<(std::ostream &os, const Ray &r) {
+	os << "Ray[" << r.o << ", " << r.d << ", " << r.mint << "," << r.maxt << "]";
+	return os;
+}
+
+class RayHit {
+public:
+	float t;
+	float b1, b2; // Barycentric coordinates of the hit point
+	unsigned int index;
+
+	void SetMiss() { index = 0xffffffffu; };
+	bool Miss() const { return (index == 0xffffffffu); };
+};
+
+}
+
+#endif	/* _LUXRAYS_RAY_H */
