f5fbb6e9991aad6487a327eb98a9920cd7e69d89
David Bucciarelli <dade@ngi.it>
2011-02-01 14:58:10 +0100

    LuxMark: minor scene change

--- samples/luxmark/ui_mainwindow.h
+++ samples/luxmark/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Tue Feb 1 12:11:31 2011
+** Created: Tue Feb 1 14:52:51 2011
 **      by: Qt User Interface Compiler version 4.6.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
