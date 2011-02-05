1297cc204c41485e30f39405e03a6d352a684740
David Bucciarelli <dade@ngi.it>
2011-02-05 10:31:49 +0100

    LuxMark: fixed message print in hybrid path render engine

--- samples/luxmark/moc_luxmarkapp.cxx
+++ samples/luxmark/moc_luxmarkapp.cxx
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'luxmarkapp.h'
 **
-** Created: Thu Feb 3 15:25:54 2011
+** Created: Sat Feb 5 10:23:22 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
