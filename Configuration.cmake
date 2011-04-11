f3d098e8ad691a565d9901b98d73c7667cc7c782
David Bucciarelli <dade@ngi.it>
2011-04-11 17:54:25 +0200

    Fixed linux OpenCL lookup

--- Configuration.cmake
+++ Configuration.cmake
@@ -41,14 +41,14 @@ IF (NOT LUXRAYS_NO_DEFAULT_CONFIG)
 
 	IF (WIN32)
 
-	 	MESSAGE(STATUS "Using default WIN32 Configuration settings")
+      MESSAGE(STATUS "Using default WIN32 Configuration settings")
 				
 	  set(BUILD_LUXMARK TRUE) # This will require QT
 			
 	  set(ENV{QTDIR} "c:/qt/")
 		
 	  set(FREEIMAGE_SEARCH_PATH     "${LuxRays_SOURCE_DIR}/../FreeImage")
-	 	set(FreeImage_INC_SEARCH_PATH "${FREEIMAGE_SEARCH_PATH}/source")
+      set(FreeImage_INC_SEARCH_PATH "${FREEIMAGE_SEARCH_PATH}/source")
 	  set(FreeImage_LIB_SEARCH_PATH "${FREEIMAGE_SEARCH_PATH}/release"
 		                                "${FREEIMAGE_SEARCH_PATH}/debug"
 		                                "${FREEIMAGE_SEARCH_PATH}/dist")
---
+++ cmake/SpecializedConfig/Config_Dade.cmake
@@ -0,0 +1,13 @@
+
+###########################################################################
+#
+# Configuration
+#
+###########################################################################
+
+MESSAGE(STATUS "Using Dade's Configuration settings")
+
+set(BOOST_SEARCH_PATH         "/home/david/projects/luxrender-dev/boost_1_43_0")
+
+set(OPENCL_SEARCH_PATH        "$ENV{ATISTREAMSDKROOT}")
+set(OPENCL_LIBRARYDIR         "${OPENCL_SEARCH_PATH}/lib/x86_64")
