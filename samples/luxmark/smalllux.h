d35ec98504c731291341274c2b7832ed40c03414
Alain Ducharme <Alain Ducharme@localhost>
2011-04-15 16:38:02 -0400

    SLGBP: minor changes + ready for Blender 2.57

---
+++ samples/luxmark/smalllux.h
@@ -0,0 +1,89 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2010 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRays.                                         *
+ *                                                                         *
+ *   LuxRays is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   LuxRays is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   LuxRays website: http://www.luxrender.net                             *
+ ***************************************************************************/
+
+#ifndef _SMALLLUX_H
+#define	_SMALLLUX_H
+
+#include <cmath>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+#if defined(__linux__) || defined(__APPLE__)
+#include <stddef.h>
+#include <sys/time.h>
+#elif defined (WIN32)
+#include <windows.h>
+#else
+	Unsupported Platform !!!
+#endif
+
+#include "luxrays/luxrays.h"
+#include "luxrays/core/utils.h"
+#include "luxrays/utils/sdl/scene.h"
+#include "luxrays/utils/film/film.h"
+#include "luxrays/utils/core/atomic.h"
+
+#include "slgcfg.h"
+
+#include "mainwindow.h"
+
+using namespace std;
+using namespace luxrays;
+using namespace luxrays::sdl;
+using namespace luxrays::utils;
+
+extern void DebugHandler(const char *msg);
+
+extern MainWindow *LogWindow;
+
+class LuxLogEvent: public QEvent {
+public:
+	LuxLogEvent(QString mesg);
+ 
+	QString getMessage() { return message; }
+
+private:
+	QString message;
+};
+
+class LuxErrorEvent: public QEvent {
+public:
+	LuxErrorEvent(QString mesg);
+
+	QString getMessage() { return message; }
+
+private:
+	QString message;
+};
+
+#define LM_LOG(a) { if (LogWindow) { std::stringstream _LM_LOG_LOCAL_SS; _LM_LOG_LOCAL_SS << a; qApp->postEvent(LogWindow, new LuxLogEvent(QString(_LM_LOG_LOCAL_SS.str().c_str()))); }}
+#define LM_LOG_LUXRAYS(a) { LM_LOG("<FONT COLOR=\"#000000\"><B>[LuxRays]</B></FONT> " << a); }
+#define LM_LOG_ENGINE(a) { LM_LOG("<FONT COLOR=\"#00ff00\"><B>[RenderEngine]</B></FONT> " << a); }
+#define LM_LOG_CFG(a) { LM_LOG("<FONT COLOR=\"#005500\"><B>[RenderConfig]</B></FONT> " << a); }
+
+#define LM_ERROR(a) { if (LogWindow) { std::stringstream _LM_ERR_LOCAL_SS; _LM_ERR_LOCAL_SS << a; qApp->postEvent(LogWindow, new LuxErrorEvent(QString(_LM_ERR_LOCAL_SS.str().c_str()))); }}
+
+enum LuxMarkAppMode {
+	BENCHMARK_OCL_GPU, BENCHMARK_OCL_CPUGPU,  BENCHMARK_OCL_CPU, BENCHMARK_NATIVE, INTERACTIVE, PAUSE
+};
+
+#endif	/* _SMALLLUX_H */
---
+++ samples/smallluxgpu/bat/RUN_SLG_SCENE_KITCHEN.bat
@@ -0,0 +1,3 @@
+@echo off
+SLG.exe scenes\kitchen\render-fast.cfg
+pause
