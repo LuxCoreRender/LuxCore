cab8ec407eeef582320fdfaa77de04213b8dac3d
David Bucciarelli <dade916@gmail.com>
2011-05-28 11:15:48 +0200

    Updated the scripts for the creation of sample packages

--- samples/smallluxgpu2/bat/RUN_SLG2_OPENCL_SIMPLE.bat
+++ samples/smallluxgpu2/bat/RUN_SLG2_OPENCL_SIMPLE.bat
@@ -1,3 +1,3 @@
 @echo off
-SLG2.exe -D renderengine.type 4 -D screen.refresh.interval 50 -D path.sampler.type INLINED_RANDOM scenes\simple\render-fast.cfg
+SLG2.exe -D renderengine.type 4 -D opencl.cpu.use 1 -D opencl.gpu.use 1 -D screen.refresh.interval 50 -D path.sampler.type INLINED_RANDOM scenes\simple\render-fast.cfg
 pause
