bc29ed1e598770f9ae811b5d1d27e6ff2b1326be
David Bucciarelli <dade916@gmail.com>
2011-12-29 17:17:34 +0100

    LuxMark: LM is now compile donly if OpenCL is available

--- samples/luxmark/moc_hardwaretree.cxx
+++ samples/luxmark/moc_hardwaretree.cxx
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'hardwaretree.h'
 **
-** Created: Thu Dec 29 16:37:34 2011
+** Created: Thu Dec 29 17:16:34 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.0)
 **
 ** WARNING! All changes made in this file will be lost!
--- samples/luxmark/moc_luxmarkapp.cxx
+++ samples/luxmark/moc_luxmarkapp.cxx
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'luxmarkapp.h'
 **
-** Created: Tue Dec 27 14:34:21 2011
+** Created: Thu Dec 29 17:16:34 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.0)
 **
 ** WARNING! All changes made in this file will be lost!
