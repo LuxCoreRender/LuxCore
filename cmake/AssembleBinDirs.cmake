69f9d564f2189f7474469e7cc0f1a20ea2bf7674
David Bucciarelli <dade@ngi.it>
2012-01-19 19:07:24 +0100

    LuxMark: packed beta2 binaries

--- cmake/AssembleBinDirs.cmake
+++ cmake/AssembleBinDirs.cmake
@@ -123,17 +123,16 @@ IF (WIN32)
 
 ELSE (WIN32)
 
-	set(LUXMARK_LINUX64_BIN_DIR "luxmark-linux64-v2.0beta1")
-	set(LUXMARK_WIN32_BIN_DIR "luxmark-win32-v2.0beta1")
+	set(LUXMARK_LINUX64_BIN_DIR "luxmark-linux64-v2.0beta2")
+	set(LUXMARK_WIN32_BIN_DIR "luxmark-win32-v2.0beta2")
 
 	# Win32
-
 	add_custom_command(
 	    OUTPUT "${LUXMARK_WIN32_BIN_DIR}"
 	    COMMAND rm -rf ${LUXMARK_WIN32_BIN_DIR}
 	    COMMAND mkdir ${LUXMARK_WIN32_BIN_DIR}
 	    COMMAND cp -r samples/luxmark/scenes ${LUXMARK_WIN32_BIN_DIR}
-	    COMMAND cp samples/luxmark/exe/*.* ${LUXMARK_WIN32_BIN_DIR}
+	    COMMAND cp samples/luxmark/exe-32bit/*.* ${LUXMARK_WIN32_BIN_DIR}
 		COMMAND mv ${LUXMARK_WIN32_BIN_DIR}/LuxMark.exe ${LUXMARK_WIN32_BIN_DIR}/LuxMark-win32.exe
 	    COMMAND cp AUTHORS.txt COPYING.txt README.txt ${LUXMARK_WIN32_BIN_DIR}
 	    COMMENT "Building ${LUXMARK_WIN32_BIN_DIR}")
@@ -146,7 +145,6 @@ ELSE (WIN32)
 	    COMMENT "Building ${LUXMARK_WIN32_BIN_DIR}.zip")
 	
 	# Linux64
-	
 	add_custom_command(
 	    OUTPUT "${LUXMARK_LINUX64_BIN_DIR}"
 	    COMMAND rm -rf ${LUXMARK_LINUX64_BIN_DIR}
@@ -154,6 +152,7 @@ ELSE (WIN32)
 	    COMMAND cp -r samples/luxmark/scenes ${LUXMARK_LINUX64_BIN_DIR}
 	    COMMAND cp bin/luxmark ${LUXMARK_LINUX64_BIN_DIR}/luxmark-linux64
 	    COMMAND cp AUTHORS.txt COPYING.txt README.txt ${LUXMARK_LINUX64_BIN_DIR}
+		COMMAND cp samples/luxmark/linux-64bit/* ${LUXMARK_LINUX64_BIN_DIR}
 	    COMMENT "Building ${LUXMARK_LINUX64_BIN_DIR}")
 	
 	add_custom_command(
