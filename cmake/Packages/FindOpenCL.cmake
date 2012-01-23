2f852f216e3fc1aa66746318e1d87bcd0902e0ea
Jens Verwiebe <Jens Verwiebe@localhost>
2012-01-23 11:26:28 +0100

    Rollback also last commit, issue was due wrong settings in users specialized config, so not needed anymore

--- cmake/Packages/FindOpenCL.cmake
+++ cmake/Packages/FindOpenCL.cmake
@@ -6,49 +6,45 @@
 # OPENCL_LIBRARY
 # 
 
-if (NOT LUXRAYS_DISABLE_OPENCL)
+IF (WIN32)
+	FIND_PATH( OPENCL_INCLUDE_PATH CL/cl.h
+		${OPENCL_ROOT}/include
+		${LuxRays_SOURCE_DIR}/../opencl
+		DOC "The directory where CL/cl.h resides")
+	FIND_LIBRARY( OPENCL_LIBRARY
+		NAMES OpenCL.lib
+		PATHS
+		${OPENCL_LIBRARYDIR}
+		${OPENCL_ROOT}/lib/
+		${LuxRays_SOURCE_DIR}/../opencl/
+		DOC "The OpenCL library")
+ELSE (WIN32)
+	FIND_PATH( OPENCL_INCLUDE_PATH CL/cl.h
+		/usr/include
+		/usr/local/include
+		/sw/include
+		/opt/local/include
+		${OPENCL_ROOT}/include
+		/usr/src/opencl-sdk/include
+		DOC "The directory where CL/cl.h resides")
+	FIND_LIBRARY( OPENCL_LIBRARY
+		NAMES opencl OpenCL
+		PATHS
+		/usr/lib64
+		/usr/lib
+		/usr/local/lib64
+		/usr/local/lib
+		/sw/lib
+		/opt/local/lib
+		/usr/src/opencl-sdk/lib/x86_64
+		${OPENCL_LIBRARYDIR}
+		DOC "The OpenCL library")
+ENDIF (WIN32)
 
-	IF (WIN32)
-		FIND_PATH( OPENCL_INCLUDE_PATH CL/cl.h
-			${OPENCL_ROOT}/include
-			${LuxRays_SOURCE_DIR}/../opencl
-			DOC "The directory where CL/cl.h resides")
-		FIND_LIBRARY( OPENCL_LIBRARY
-			NAMES OpenCL.lib
-			PATHS
-			${OPENCL_LIBRARYDIR}
-			${OPENCL_ROOT}/lib/
-			${LuxRays_SOURCE_DIR}/../opencl/
-			DOC "The OpenCL library")
-	ELSE (WIN32)
-		FIND_PATH( OPENCL_INCLUDE_PATH CL/cl.h
-			/usr/include
-			/usr/local/include
-			/sw/include
-			/opt/local/include
-			${OPENCL_ROOT}/include
-			/usr/src/opencl-sdk/include
-			DOC "The directory where CL/cl.h resides")
-		FIND_LIBRARY( OPENCL_LIBRARY
-			NAMES opencl OpenCL
-			PATHS
-			/usr/lib64
-			/usr/lib
-			/usr/local/lib64
-			/usr/local/lib
-			/sw/lib
-			/opt/local/lib
-			/usr/src/opencl-sdk/lib/x86_64
-			${OPENCL_LIBRARYDIR}
-			DOC "The OpenCL library")
-	ENDIF (WIN32)
+IF (OPENCL_INCLUDE_PATH)
+	SET( OPENCL_FOUND 1 CACHE STRING "Set to 1 if OpenCL is found, 0 otherwise")
+ELSE (OPENCL_INCLUDE_PATH)
+	SET( OPENCL_FOUND 0 CACHE STRING "Set to 1 if OpenCL is found, 0 otherwise")
+ENDIF (OPENCL_INCLUDE_PATH)
 
-	IF (OPENCL_INCLUDE_PATH)
-		SET( OPENCL_FOUND 1 CACHE STRING "Set to 1 if OpenCL is found, 0 otherwise")
-	ELSE (OPENCL_INCLUDE_PATH)
-		SET( OPENCL_FOUND 0 CACHE STRING "Set to 1 if OpenCL is found, 0 otherwise")
-	ENDIF (OPENCL_INCLUDE_PATH)
-
-	MARK_AS_ADVANCED( OPENCL_FOUND )
-
-endif (NOT LUXRAYS_DISABLE_OPENCL)
+MARK_AS_ADVANCED( OPENCL_FOUND )
