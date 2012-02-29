573a399d2ea76779fa5183f6e239fcb59bcf6f58
David Bucciarelli <dade@ngi.it>
2012-02-29 11:30:40 +0100

    Minor update of cmake/SpecializedConfig/Config_Dade.cmake

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -41,7 +41,7 @@ set(BOOST_ROOT                  "${BOOST_SEARCH_PATH}")
 #set(Boost_DEBUG                 ON)
 set(Boost_MINIMUM_VERSION       "1.43.0")
 
-set(Boost_ADDITIONAL_VERSIONS "1.47.0" "1.46.1" "1.46" "1.46.0" "1.45" "1.45.0" "1.44" "1.44.0" "1.43" "1.43.0" )
+set(Boost_ADDITIONAL_VERSIONS "1.47.0" "1.46.1" "1.46" "1.46.0" "1.45" "1.45.0" "1.44" "1.44.0" "1.43" "1.43.0")
 
 set(LUXRAYS_BOOST_COMPONENTS thread filesystem system)
 find_package(Boost ${Boost_MINIMUM_VERSION} COMPONENTS ${LUXRAYS_BOOST_COMPONENTS})
--- cmake/SpecializedConfig/Config_Dade.cmake
+++ cmake/SpecializedConfig/Config_Dade.cmake
@@ -9,10 +9,10 @@
 
 MESSAGE(STATUS "Using Dade's Linux Configuration settings")
 
-set(BOOST_SEARCH_PATH         "/home/david/projects/luxrender-dev/boost_1_43_0")
+#set(BOOST_SEARCH_PATH         "/home/david/projects/luxrender-dev/boost_1_43_0")
 
 set(OPENCL_SEARCH_PATH        "$ENV{ATISTREAMSDKROOT}")
-set(OPENCL_INCLUDEPATH         "${OPENCL_SEARCH_PATH}/include")
+set(OPENCL_INCLUDEPATH        "${OPENCL_SEARCH_PATH}/include")
 #set(OPENCL_LIBRARYDIR         "${OPENCL_SEARCH_PATH}/lib/x86_64")
 
 #set(BUILD_LUXMARK TRUE)
