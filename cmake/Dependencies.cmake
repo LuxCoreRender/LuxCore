f90cc2ac27ea46635442293f0544d3045f37615e
Omni Flux <omniflux@omniflux.com>
2012-04-12 21:40:59 -0600

    Update to Boost filesystem version 3

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -39,9 +39,9 @@ set(Boost_USE_MULTITHREADED     ON)
 set(Boost_USE_STATIC_RUNTIME    OFF)
 set(BOOST_ROOT                  "${BOOST_SEARCH_PATH}")
 #set(Boost_DEBUG                 ON)
-set(Boost_MINIMUM_VERSION       "1.43.0")
+set(Boost_MINIMUM_VERSION       "1.44.0")
 
-set(Boost_ADDITIONAL_VERSIONS "1.47.0" "1.46.1" "1.46" "1.46.0" "1.45" "1.45.0" "1.44" "1.44.0" "1.43" "1.43.0")
+set(Boost_ADDITIONAL_VERSIONS "1.47.0" "1.46.1" "1.46" "1.46.0" "1.45" "1.45.0" "1.44" "1.44.0")
 
 set(LUXRAYS_BOOST_COMPONENTS thread filesystem system)
 find_package(Boost ${Boost_MINIMUM_VERSION} COMPONENTS ${LUXRAYS_BOOST_COMPONENTS})
@@ -58,8 +58,10 @@ endif()
 if (Boost_FOUND)
   include_directories(${Boost_INCLUDE_DIRS})
   link_directories(${Boost_LIBRARY_DIRS})
-  # Don't use latest boost versions interfaces
-  ADD_DEFINITIONS(-DBOOST_FILESYSTEM_VERSION=2)
+  # Don't use old boost versions interfaces
+  ADD_DEFINITIONS(-DBOOST_FILESYSTEM_NO_DEPRECATED)
+  # Retain compatibility with Boost 1.44 and 1.45
+  ADD_DEFINITIONS(-DBOOST_FILESYSTEM_VERSION=3)
 endif ()
 
 
