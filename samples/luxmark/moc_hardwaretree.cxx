0acc292150a8ac165a2e9a94572cb550ee5d160c
David Bucciarelli <dade916@gmail.com>
2012-04-19 10:38:23 +0200

    Updated LuxMark scenes to take account of the change in default texture map gamma

--- samples/luxmark/moc_hardwaretree.cxx
+++ samples/luxmark/moc_hardwaretree.cxx
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'hardwaretree.h'
 **
-** Created: Tue Feb 14 22:25:50 2012
+** Created: Thu Apr 19 10:20:59 2012
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
 **
 ** WARNING! All changes made in this file will be lost!
