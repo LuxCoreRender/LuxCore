71e1435b34960d219aa37cbf26c1bf9e8e335e06
Jens Verwiebe <Jens Verwiebe@localhost>
2013-01-17 09:26:18 +0100

    Change the opencl check to look at device not platform

--- include/luxrays/opencl/device.h
+++ include/luxrays/opencl/device.h
@@ -76,19 +76,19 @@ public:
 	bool IsOpenCL_1_0() const {
 		cl::Platform platform = oclDevice.getInfo<CL_DEVICE_PLATFORM>();
 		int major, minor;
-		sscanf(platform.getInfo<CL_PLATFORM_VERSION>().c_str(), "OpenCL %d.%d", &major, &minor);
+		sscanf(oclDevice.getInfo<CL_DEVICE_VERSION>().c_str(), "OpenCL %d.%d", &major, &minor);
 		return (major >= 1) && (minor >= 0);
 	}
 	bool IsOpenCL_1_1() const {
 		cl::Platform platform = oclDevice.getInfo<CL_DEVICE_PLATFORM>();
 		int major, minor;
-		sscanf(platform.getInfo<CL_PLATFORM_VERSION>().c_str(), "OpenCL %d.%d", &major, &minor);
+		sscanf(oclDevice.getInfo<CL_DEVICE_VERSION>().c_str(), "OpenCL %d.%d", &major, &minor);
 		return (major >= 1) && (minor >= 1);
 	}
 	bool IsOpenCL_1_2() const {
 		cl::Platform platform = oclDevice.getInfo<CL_DEVICE_PLATFORM>();
 		int major, minor;
-		sscanf(platform.getInfo<CL_PLATFORM_VERSION>().c_str(), "OpenCL %d.%d", &major, &minor);
+		sscanf(oclDevice.getInfo<CL_DEVICE_VERSION>().c_str(), "OpenCL %d.%d", &major, &minor);
 		return (major >= 1) && (minor >= 2);
 	}
 
