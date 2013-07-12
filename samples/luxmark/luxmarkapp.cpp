a71e82d866bd7c754d237078c5d5638cc2070a8d
David Bucciarelli <dade916@gmail.com>
2013-07-12 18:10:09 +0200

    FreeImageErrorHandler() is now declared static in order to avoid conficts among multiple functions defined, with the same name, inside LuxRays, SLG and Lux libraries

--- samples/luxmark/luxmarkapp.cpp
+++ samples/luxmark/luxmarkapp.cpp
@@ -30,7 +30,7 @@
 #include "resultdialog.h"
 #include "slgdefs.h"
 
-void FreeImageErrorHandler(FREE_IMAGE_FORMAT fif, const char *message) {
+static void FreeImageErrorHandler(FREE_IMAGE_FORMAT fif, const char *message) {
 	printf("\n*** ");
 	if(fif != FIF_UNKNOWN)
 		printf("%s Format\n", FreeImage_GetFormatFromFIF(fif));
