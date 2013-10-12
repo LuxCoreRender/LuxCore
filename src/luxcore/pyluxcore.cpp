5caa6ede518bd215d69072fadad0756fa56d8e59
David Bucciarelli <dade916@gmail.com>
2013-10-12 19:34:01 +0200

    Initial work on PyLuxCore

--- src/luxcore/pyluxcore.cpp
+++ src/luxcore/pyluxcore.cpp
@@ -19,8 +19,59 @@
  *   LuxRays website: http://www.luxrender.net                             *
  ***************************************************************************/
 
-#include "luxcore/luxcore.h"
+#include <Python.h>
+#include <boost/python.hpp>
+
+#include <luxcore/luxcore.h>
 
 using namespace std;
 using namespace luxrays;
 using namespace luxcore;
+
+namespace luxcore {
+
+static const char *LuxCoreVersion() {
+	static const char *luxCoreVersion = LUXCORE_VERSION_MAJOR "." LUXCORE_VERSION_MINOR;
+	return luxCoreVersion;
+}
+
+BOOST_PYTHON_MODULE(pyluxcore) {
+	using namespace boost::python;
+
+	docstring_options doc_options(
+		true,	// Show user defined docstrings
+		true,	// Show python signatures
+		false	// Show c++ signatures
+	);
+
+	//This 'module' is actually a fake package
+	object package = scope();
+	package.attr("__path__") = "pyluxcore";
+	package.attr("__package__") = "pyluxcore";
+	package.attr("__doc__") = "New LuxRender Python bindings\n\n"
+			"Provides access to the new LuxRender API in python\n\n";
+
+	def("version", LuxCoreVersion, "Returns the LuxCore version");
+
+	//--------------------------------------------------------------------------
+	// Property class
+	//--------------------------------------------------------------------------
+
+    class_<Property>("Property", init<string>())
+		.def(init<string, bool>())
+		.def(init<string, int>())
+		.def(init<string, double>())
+		.def(init<string, string>())
+
+		.def("GetName", &Property::GetName, return_value_policy<copy_const_reference>())
+		.def("GetSize", &Property::GetSize)
+		.def("Clear", &Property::Clear)
+
+		.def("Get", &Property::Get<bool>)
+		.def("Get", &Property::Get<int>)
+		.def("Get", &Property::Get<double>)
+		.def("Get", &Property::Get<string>)
+    ;
+}
+
+}
