7216427e4b150df9002df12fadf27a101b82ac76
david <david@i7-3930k>
2013-10-28 13:40:05 +0100

    Compiled on Win64

--- cmake/SpecializedConfig/Config_Dade-win64.cmake
+++ cmake/SpecializedConfig/Config_Dade-win64.cmake
@@ -10,7 +10,9 @@
 # Boost:
 #  CALL "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
 #  bootstrap.bat
-#  bjam --toolset=msvc address-model=64 --stagedir=stage stage -j 12
+#  (with Python) path %PATH%;c:\Python27
+#  (with Python) bjam --with-python --toolset=msvc address-model=64 --stagedir=stage stage -j 12
+#  (without Python) bjam --toolset=msvc address-model=64 --stagedir=stage stage -j 12
 #
 # To disable OpenMP support (VCOMP.lib error) in Visula Studio 2010 go
 # properties => C/C++ => languages and disable OpenMP
@@ -26,8 +28,9 @@ set(FreeImage_INC_SEARCH_PATH "${FREEIMAGE_SEARCH_PATH}/Dist")
 set(FreeImage_LIB_SEARCH_PATH "${FREEIMAGE_SEARCH_PATH}/Dist")
 set(FREEIMAGE_ROOT_DIR "${FREEIMAGE_SEARCH_PATH}/Dist")
 
+#set(Boost_DEBUG 1)
 set(BOOST_SEARCH_PATH         "E:/projects/luxrays-64bit/boost_1_48_0")
-set(BOOST_LIBRARYDIR          "${BOOST_SEARCH_PATH}/stage/boost/lib")
+set(BOOST_LIBRARYDIR          "${BOOST_SEARCH_PATH}/stage/lib")
 
 set(OPENCL_SEARCH_PATH        "C:/Program Files (x86)/AMD APP")
 set(OPENCL_LIBRARYDIR         "${OPENCL_SEARCH_PATH}/lib/x86")
