25d294063f585022e8b9b5409778345335b1b1c6
Jens Verwiebe <Jens Verwiebe@localhost>
2014-01-20 16:42:31 +0100

    Fix OSX compiling aagain after find modules removal, move new lib config to specialized config

---
+++ cmake/SpecializedConfig/Config_Dade.cmake
@@ -0,0 +1,25 @@
+
+###########################################################################
+#
+# Configuration
+#
+###########################################################################
+
+#cmake -DLUXRAYS_CUSTOM_CONFIG=cmake/SpecializedConfig/Config_Dade.cmake .
+
+MESSAGE(STATUS "Using Dade's Linux Configuration settings")
+
+# To compile with a specific Python version
+#set(Python_ADDITIONAL_VERSIONS "2.6")
+
+#set(BOOST_SEARCH_PATH         "/home/david/projects/luxrender-dev/boost_1_53_0")
+set(OPENIMAGEIO_ROOT_DIR        "/home/david/projects/luxrender-dev/oiio-RB-1.2")
+
+set(OPENCL_SEARCH_PATH        "$ENV{ATISTREAMSDKROOT}")
+set(OPENCL_INCLUDEPATH        "${OPENCL_SEARCH_PATH}/include")
+#set(OPENCL_LIBRARYDIR         "${OPENCL_SEARCH_PATH}/lib/x86_64")
+
+#set(LUXRAYS_DISABLE_OPENCL TRUE)
+
+#set(CMAKE_BUILD_TYPE "Debug")
+set(CMAKE_BUILD_TYPE "Release")
