4ce34c02b9e8eae1fd4e76ef655d1b28f2c3d05a
neo2068 <neo2068@web.de>
2014-03-30 19:57:20 +0200

    Add blender blend texture to CPU renderengines

---
+++ include/slg/sdl/blender_texture.h
@@ -0,0 +1,216 @@
+/***************************************************************************
+ * Copyright 1998-2013 by authors (see AUTHORS.txt)                        *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ * Licensed under the Apache License, Version 2.0 (the "License");         *
+ * you may not use this file except in compliance with the License.        *
+ * You may obtain a copy of the License at                                 *
+ *                                                                         *
+ *     http://www.apache.org/licenses/LICENSE-2.0                          *
+ *                                                                         *
+ * Unless required by applicable law or agreed to in writing, software     *
+ * distributed under the License is distributed on an "AS IS" BASIS,       *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ * See the License for the specific language governing permissions and     *
+ * limitations under the License.                                          *
+ ***************************************************************************/
+
+#ifndef _SLG_BLENDER_TEXTURE_H
+#define	_SLG_BLENDER_TEXTURE_H
+
+#include "slg/sdl/texture.h"
+
+namespace slg {
+
+typedef enum {
+    BLENDER_ORIGINAL, ORIGINAL_PERLIN, IMPROVED_PERLIN,
+    VORONOI_F1, VORONOI_F2, VORONOI_F3, VORONOI_F4, VORONOI_F2_F1,
+    VORONOI_CRACKLE, CELL_NOISE
+} BlenderNoiseBasis;
+
+//------------------------------------------------------------------------------
+// Blender blend texture
+//------------------------------------------------------------------------------
+typedef enum {
+	TEX_LIN, TEX_QUAD, TEX_EASE, TEX_DIAG, TEX_SPHERE, TEX_HALO, TEX_RAD
+} ProgressionType;
+
+
+class BlenderBlendTexture : public Texture {
+public:
+	BlenderBlendTexture(const TextureMapping3D *mp, const ProgressionType type, const bool direction, float bright, float contrast);
+	virtual ~BlenderBlendTexture() { delete mapping; }
+
+	virtual TextureType GetType() const { return BLENDER_BLEND; }
+	virtual float GetFloatValue(const HitPoint &hitPoint) const;
+	virtual luxrays::Spectrum GetSpectrumValue(const HitPoint &hitPoint) const;
+	// The following methods don't make very much sense in this case. I have no
+	// information about the color.
+	virtual float Y() const { return .5f; }
+	virtual float Filter() const { return .5f; }
+
+	const TextureMapping3D *GetTextureMapping() const { return mapping; }
+	ProgressionType GetProgressionType() const { return type; }
+	bool GetDirection() const { return direction; }
+	float GetBright() const { return bright; }
+	float GetContrast() const { return contrast; }
+
+	virtual luxrays::Properties ToProperties(const ImageMapCache &imgMapCache) const;
+
+private:
+	const TextureMapping3D *mapping;
+	ProgressionType type;
+	bool direction;
+	float bright, contrast;
+};
+
+//------------------------------------------------------------------------------
+// Blender wood texture
+//------------------------------------------------------------------------------
+
+typedef enum {
+	BANDS, RINGS, BANDNOISE, RINGNOISE
+} BlenderWoodType;
+
+typedef enum {
+	TEX_SIN, TEX_SAW, TEX_TRI
+} BlenderWoodNoiseBase;
+
+class BlenderWoodTexture : public Texture {
+public:
+	BlenderWoodTexture(const TextureMapping3D *mp, const std::string &ptype, const std::string &pnoise, const float noisesize, float turb, bool hard, float bright, float contrast);
+	virtual ~BlenderWoodTexture() { delete mapping; }
+
+	virtual TextureType GetType() const { return BLENDER_WOOD; }
+	virtual float GetFloatValue(const HitPoint &hitPoint) const;
+	virtual luxrays::Spectrum GetSpectrumValue(const HitPoint &hitPoint) const;
+	// The following methods don't make very much sense in this case. I have no
+	// information about the color.
+	virtual float Y() const { return .5f; }
+	virtual float Filter() const { return .5f; }
+
+	const TextureMapping3D *GetTextureMapping() const { return mapping; }
+	BlenderWoodType GetWoodType() const { return type; }
+	BlenderWoodNoiseBase GetNoiseBasis2() const { return noisebasis2; }
+	float GetNoiseSize() const { return noisesize; }
+	float GetTurbulence() const { return turbulence; }
+	float GetBright() const { return bright; }
+	float GetContrast() const { return contrast; }
+	bool GetNoiseType() const { return hard; }
+
+	virtual luxrays::Properties ToProperties(const ImageMapCache &imgMapCache) const;
+
+private:
+	const TextureMapping3D *mapping;
+	BlenderWoodType type;
+	BlenderWoodNoiseBase noisebasis2;	
+	float noisesize, turbulence;
+	bool hard;
+	float bright, contrast;
+};
+
+//------------------------------------------------------------------------------
+// Blender clouds texture
+//------------------------------------------------------------------------------
+
+class BlenderCloudsTexture : public Texture {
+public:
+	BlenderCloudsTexture(const TextureMapping3D *mp, const float noisesize, const int noisedepth, bool hard, float bright, float contrast);
+	virtual ~BlenderCloudsTexture() { delete mapping; }
+
+	virtual TextureType GetType() const { return BLENDER_CLOUDS; }
+	virtual float GetFloatValue(const HitPoint &hitPoint) const;
+	virtual luxrays::Spectrum GetSpectrumValue(const HitPoint &hitPoint) const;
+	// The following methods don't make very much sense in this case. I have no
+	// information about the color.
+	virtual float Y() const { return .5f; }
+	virtual float Filter() const { return .5f; }
+
+	const TextureMapping3D *GetTextureMapping() const { return mapping; }
+	float GetBright() const { return bright; }
+	float GetContrast() const { return contrast; }
+	float GetNoiseSize() const { return noisesize; }
+	int GetNoiseDepth() const { return noisedepth; }
+	bool GetNoiseType() const { return hard; }
+
+	virtual luxrays::Properties ToProperties(const ImageMapCache &imgMapCache) const;
+
+private:
+	const TextureMapping3D *mapping;
+	int noisedepth;	
+	float noisesize;	
+	bool hard;
+	float bright, contrast;
+};
+
+//------------------------------------------------------------------------------
+// Blender voronoi texture
+//------------------------------------------------------------------------------
+
+class BlenderVoronoiTexture : public Texture {
+public:
+	BlenderVoronoiTexture(const TextureMapping3D *mp, const float intensity, const float exponent, const float fw1, const float fw2, const float fw3, const float fw4, 
+		const DistanceMetric distmetric, const float noisesize, float bright, float contrast);
+	virtual ~BlenderVoronoiTexture() { delete mapping; }
+
+	virtual TextureType GetType() const { return BLENDER_VORONOI; }
+	virtual float GetFloatValue(const HitPoint &hitPoint) const;
+	virtual luxrays::Spectrum GetSpectrumValue(const HitPoint &hitPoint) const;
+	// The following methods don't make very much sense in this case. I have no
+	// information about the color.
+	virtual float Y() const { return .5f; }
+	virtual float Filter() const { return .5f; }
+
+	const TextureMapping3D *GetTextureMapping() const { return mapping; }
+	float GetBright() const { return bright; }
+	float GetContrast() const { return contrast; }
+	string GetDistMetric() const {
+		std::string result = "";
+		switch(distancemetric) {
+			case DISTANCE_SQUARED:
+				result = "distance_squared";
+			case MANHATTAN:
+				result = "manhattan";
+			case CHEBYCHEV:
+				result = "chebychev";
+			case MINKOWSKI_HALF:
+				result = "minkowski_half";
+			case MINKOWSKI_FOUR:
+				result = "minkowski_four";
+			case MINKOWSKI:
+				result = "minkowski";
+			case ACTUAL_DISTANCE: 
+			default:
+				result = "actual_distance";
+
+			return result;
+		}
+	}
+	float GetFeatureWeight1() const { return feature_weight1; }
+	float GetFeatureWeight2() const { return feature_weight2; }
+	float GetFeatureWeight3() const { return feature_weight3; }
+	float GetFeatureWeight4() const { return feature_weight4; }
+	float GetNoiseSize() const { return noisesize; }
+	float GetIntensity() const { return intensity; }
+	float GetExponent() const { return exponent; }
+
+	virtual luxrays::Properties ToProperties(const ImageMapCache &imgMapCache) const;
+
+private:
+	const TextureMapping3D *mapping;
+
+	float intensity;
+	float feature_weight1;
+	float feature_weight2;
+	float feature_weight3;
+	float feature_weight4;
+	DistanceMetric distancemetric;
+	float exponent;
+	float noisesize;
+	float bright, contrast;
+};
+
+}
+
+#endif	/* _SLG_BLENDER_TEXTURE_H */
---
+++ src/slg/sdl/blender_texture.cpp
@@ -0,0 +1,358 @@
+/***************************************************************************
+ * Copyright 1998-2013 by authors (see AUTHORS.txt)                        *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ * Licensed under the Apache License, Version 2.0 (the "License");         *
+ * you may not use this file except in compliance with the License.        *
+ * You may obtain a copy of the License at                                 *
+ *                                                                         *
+ *     http://www.apache.org/licenses/LICENSE-2.0                          *
+ *                                                                         *
+ * Unless required by applicable law or agreed to in writing, software     *
+ * distributed under the License is distributed on an "AS IS" BASIS,       *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ * See the License for the specific language governing permissions and     *
+ * limitations under the License.                                          *
+ ***************************************************************************/
+
+#include "slg/sdl/sdl.h"
+#include "slg/sdl/bsdf.h"
+#include "slg/sdl/blender_texture.h"
+
+using namespace luxrays;
+using namespace slg;
+
+//------------------------------------------------------------------------------
+// Blender blend texture
+//------------------------------------------------------------------------------
+
+BlenderBlendTexture::BlenderBlendTexture(const TextureMapping3D *mp, const ProgressionType type, 
+										 const bool direction, float bright, float contrast) : 
+		mapping(mp), type(type), direction(direction), bright(bright), contrast(contrast) {
+
+}
+
+float BlenderBlendTexture::GetFloatValue(const HitPoint &hitPoint) const {
+	float result = 0.f;
+	Point P(mapping->Map(hitPoint));
+
+	float x, y, t;
+    
+	if(direction) {
+		//horizontal
+		x = P.x;
+		y = P.y;
+	} else {
+		//vertical
+		x = P.y;
+		y = P.x;
+	};
+
+
+    if (type == TEX_LIN) { /* lin */
+        result = (1.f + x) / 2.f;
+    } else if (type == TEX_QUAD) { /* quad */
+        result = (1.f + x) / 2.f;
+        if (result < 0.f) result = 0.f;
+        else result *= result;
+    } else if (type == TEX_EASE) { /* ease */
+        result = (1.f + x) / 2.f;
+        if (result <= 0.f) result = 0.f;
+        else if (result >= 1.f) result = 1.f;
+        else {
+            t = result * result;
+            result = (3.f * t - 2.f * t * result);
+        }
+    } else if (type == TEX_DIAG) { /* diag */
+        result = (2.f + x + y) / 4.f;
+    } else if (type == TEX_RAD) { /* radial */
+        result = (atan2f(y, x) / (2.f * M_PI) + 0.5f);
+    } else { /* sphere TEX_SPHERE */
+        result = 1.f - sqrt(x * x + y * y + P.z * P.z);
+        if (result < 0.f) result = 0.f;
+        if (type == TEX_HALO) result *= result; /* halo */
+    }
+
+	result = (result - 0.5f) * contrast + bright - 0.5f;
+    if(result < 0.f) result = 0.f; 
+	else if(result > 1.f) result = 1.f;
+
+	return result;
+}
+
+Spectrum BlenderBlendTexture::GetSpectrumValue(const HitPoint &hitPoint) const {
+	return Spectrum(GetFloatValue(hitPoint));
+}
+
+Properties BlenderBlendTexture::ToProperties(const ImageMapCache &imgMapCache) const {
+	Properties props;
+
+	std::string progressiontype;
+	switch(type) {
+		default:
+		case TEX_LIN:
+			progressiontype = "linear";
+			break;
+		case TEX_QUAD:
+			progressiontype = "quadratic";
+			break;
+		case TEX_EASE:
+			progressiontype = "easing";
+			break;
+		case TEX_DIAG:
+			progressiontype = "diagonal";
+			break;
+		case TEX_SPHERE:
+			progressiontype = "spherical";
+			break;
+		case TEX_HALO:
+			progressiontype = "quadratic_spherical";
+			break;
+		case TEX_RAD:
+			progressiontype = "radial";
+			break;
+	}
+	std::string directiontype = "horizontal";
+	if(direction) directiontype = "vertical";
+
+	const std::string name = GetName();
+
+	props.Set(Property("scene.textures." + name + ".type")("blender_blend"));
+	props.Set(Property("scene.textures." + name + ".progressiontype")(progressiontype));
+	props.Set(Property("scene.textures." + name + ".direction")(directiontype));
+	props.Set(Property("scene.textures." + name + ".bright")(bright));
+	props.Set(Property("scene.textures." + name + ".contrast")(contrast));
+	props.Set(mapping->ToProperties("scene.textures." + name + ".mapping"));
+
+	return props;
+}
+
+//------------------------------------------------------------------------------
+// Blender wood texture
+//------------------------------------------------------------------------------
+
+BlenderWoodTexture::BlenderWoodTexture(const TextureMapping3D *mp, const std::string &ptype, const std::string &pnoise,
+		const float noisesize, float turb, bool hard, float bright, float contrast) : 
+		mapping(mp), type(BANDS), noisebasis2(TEX_SIN),  noisesize(noisesize),
+		turbulence(turb), hard(hard), bright(bright), contrast(contrast) {
+	if(ptype == "bands") {
+		type = BANDS;
+	} else if(ptype == "rings") {
+		type = RINGS;
+	} else if(ptype == "bandnoise") {
+		type = BANDNOISE;
+	} else if(ptype == "ringnoise") {
+		type = RINGNOISE;
+	};
+
+	if(pnoise == "sin") {
+		noisebasis2 = TEX_SIN;
+	} else if(pnoise == "saw") {
+		noisebasis2 = TEX_SAW;
+	} else if(pnoise == "tri") {
+		noisebasis2 = TEX_TRI;
+	};
+}
+
+float BlenderWoodTexture::GetFloatValue(const HitPoint &hitPoint) const {
+	Point P(mapping->Map(hitPoint));
+	float scale = 1.f;
+	if(fabs(noisesize) > 0.00001f) scale = (1.f/noisesize);
+
+    float (*waveform[3])(float); /* create array of pointers to waveform functions */
+    waveform[0] = tex_sin; /* assign address of tex_sin() function to pointer array */
+    waveform[1] = tex_saw;
+    waveform[2] = tex_tri;
+
+	u_int wf = 0;
+	if(noisebasis2 == TEX_SAW) { 
+		wf = 1;
+	} else if(noisebasis2 == TEX_TRI) 
+		wf = 2;
+
+	float wood = 0.f;
+	switch(type) {
+		case BANDS:
+			wood = waveform[wf]((P.x + P.y + P.z) * 10.f);
+			break;
+		case RINGS:
+			wood = waveform[wf](sqrtf(P.x*P.x + P.y*P.y + P.z*P.z) * 20.f);
+			break;
+		case BANDNOISE:
+			if(hard) wood = turbulence * fabs(2.f * Noise(scale*P) - 1.f);			
+			else wood = turbulence * Noise(scale*P);
+			wood = waveform[wf]((P.x + P.y + P.z) * 10.f + wood);
+			break;
+		case RINGNOISE:
+			if(hard) wood = turbulence * fabs(2.f * Noise(scale*P) - 1.f);			
+			else wood = turbulence * Noise(scale*P);
+			wood = waveform[wf](sqrtf(P.x*P.x + P.y*P.y + P.z*P.z) * 20.f + wood);
+			break;
+	}
+    
+	wood = (wood - 0.5f) * contrast + bright - 0.5f;
+    if(wood < 0.f) wood = 0.f; 
+	else if(wood > 1.f) wood = 1.f;
+	
+    return wood;
+}
+
+Spectrum BlenderWoodTexture::GetSpectrumValue(const HitPoint &hitPoint) const {
+	return Spectrum(GetFloatValue(hitPoint));
+}
+
+Properties BlenderWoodTexture::ToProperties(const ImageMapCache &imgMapCache) const {
+	Properties props;
+
+	std::string noise;
+	switch(noisebasis2) {
+		default:
+		case TEX_SIN:
+			noise = "sin";
+			break;
+		case TEX_SAW:
+			noise = "saw";
+			break;
+		case TEX_TRI:
+			noise = "tri";
+			break;
+	}
+	std::string woodtype;
+	switch(type) {
+		default:
+		case BANDS:
+			woodtype = "bands";
+			break;
+		case RINGS:
+			woodtype = "rings";
+			break;
+		case BANDNOISE:
+			woodtype = "bandnoise";
+			break;
+		case RINGNOISE:
+			woodtype = "ringnoise";
+			break;
+	}
+	std::string noisetype = "soft_noise";
+	if(hard) noisetype = "hard_noise";
+
+	const std::string name = GetName();
+
+	props.Set(Property("scene.textures." + name + ".type")("blender_wood"));
+	props.Set(Property("scene.textures." + name + ".woodtype")(woodtype));
+	props.Set(Property("scene.textures." + name + ".noisebasis2")(noise));
+	props.Set(Property("scene.textures." + name + ".noisesize")(noisesize));
+	props.Set(Property("scene.textures." + name + ".noisetype")(noisetype));
+	props.Set(Property("scene.textures." + name + ".turbulence")(turbulence));
+	props.Set(Property("scene.textures." + name + ".bright")(bright));
+	props.Set(Property("scene.textures." + name + ".contrast")(contrast));
+	props.Set(mapping->ToProperties("scene.textures." + name + ".mapping"));
+
+	return props;
+}
+
+//------------------------------------------------------------------------------
+// Blender clouds texture
+//------------------------------------------------------------------------------
+
+BlenderCloudsTexture::BlenderCloudsTexture(const TextureMapping3D *mp, const float noisesize, const int noisedepth, 
+		bool hard, float bright, float contrast) : 
+		mapping(mp), noisedepth(noisedepth), noisesize(noisesize), hard(hard), bright(bright), contrast(contrast) {
+}
+
+float BlenderCloudsTexture::GetFloatValue(const HitPoint &hitPoint) const {
+	Point P(mapping->Map(hitPoint));
+	float scale = 1.f;
+	if(fabs(noisesize) > 0.00001f) scale = (1.f/noisesize);
+
+	float clouds = Turbulence(scale*P, noisesize, noisedepth);
+
+	clouds = (clouds - 0.5f) * contrast + bright - 0.5f;
+    if(clouds < 0.f) clouds = 0.f; 
+	else if(clouds > 1.f) clouds = 1.f;
+	
+    return clouds;
+}
+
+Spectrum BlenderCloudsTexture::GetSpectrumValue(const HitPoint &hitPoint) const {
+	return Spectrum(GetFloatValue(hitPoint));
+}
+
+Properties BlenderCloudsTexture::ToProperties(const ImageMapCache &imgMapCache) const {
+	Properties props;
+
+	std::string noisetype = "soft_noise";
+	if(hard) noisetype = "hard_noise";
+
+	const std::string name = GetName();
+
+	props.Set(Property("scene.textures." + name + ".type")("blender_clouds"));
+	props.Set(Property("scene.textures." + name + ".noisesize")(noisesize));
+	props.Set(Property("scene.textures." + name + ".noisedepth")(noisedepth));
+	props.Set(Property("scene.textures." + name + ".bright")(bright));
+	props.Set(Property("scene.textures." + name + ".contrast")(contrast));
+	props.Set(mapping->ToProperties("scene.textures." + name + ".mapping"));
+
+	return props;
+}
+
+//------------------------------------------------------------------------------
+// Blender voronoi texture
+//------------------------------------------------------------------------------
+
+BlenderVoronoiTexture::BlenderVoronoiTexture(const TextureMapping3D *mp, const float intensity, const float exponent,
+        const float fw1, const float fw2, const float fw3, const float fw4, DistanceMetric distmetric, float noisesize,  float bright, float contrast) :
+		mapping(mp), intensity(intensity), feature_weight1(fw1), feature_weight2(fw2), feature_weight3(fw3), feature_weight4(fw4),
+		distancemetric(distmetric), exponent(exponent),
+		noisesize(noisesize), bright(bright), contrast(contrast) {
+}
+
+float BlenderVoronoiTexture::GetFloatValue(const HitPoint &hitPoint) const {
+    float da[4], pa[12]; /* distance and point coordinate arrays of 4 nearest neighbours */
+	luxrays::Point P(mapping->Map(hitPoint));
+
+    const float aw1 = fabs(feature_weight1);
+    const float aw2 = fabs(feature_weight2);
+    const float aw3 = fabs(feature_weight3);
+    const float aw4 = fabs(feature_weight4);
+
+    float sc = (aw1 + aw2 + aw3 + aw4);
+
+    if (sc > 1e-3f) sc = noisesize / sc;
+
+    float result = 1.f;
+
+	voronoi(P, da, pa, exponent, distancemetric);
+    result = sc * fabs(feature_weight1 * da[0] + feature_weight2 * da[1] + feature_weight3 * da[2] + feature_weight4 * da[3]);
+
+	result = (result - 0.5f) * contrast + bright - 0.5f;
+    if(result < 0.f) result = 0.f; 
+	else if(result > 1.f) result = 1.f;
+
+    return result;
+}
+
+Spectrum BlenderVoronoiTexture::GetSpectrumValue(const HitPoint &hitPoint) const {
+	return Spectrum(GetFloatValue(hitPoint));
+}
+
+Properties BlenderVoronoiTexture::ToProperties(const ImageMapCache &imgMapCache) const {
+	Properties props;
+
+	const std::string name = GetName();
+
+	props.Set(Property("scene.textures." + name + ".type")("blender_voronoi"));
+	props.Set(Property("scene.textures." + name + ".intentity")(intensity));
+	props.Set(Property("scene.textures." + name + ".exponent")(exponent));
+	props.Set(Property("scene.textures." + name + ".w1")(feature_weight1));
+	props.Set(Property("scene.textures." + name + ".w2")(feature_weight2));
+	props.Set(Property("scene.textures." + name + ".w3")(feature_weight3));
+	props.Set(Property("scene.textures." + name + ".w4")(feature_weight4));
+	props.Set(Property("scene.textures." + name + ".noisesize")(noisesize));
+	props.Set(Property("scene.textures." + name + ".bright")(bright));
+	props.Set(Property("scene.textures." + name + ".contrast")(contrast));
+	props.Set(mapping->ToProperties("scene.textures." + name + ".mapping"));
+
+	return props;
+}
