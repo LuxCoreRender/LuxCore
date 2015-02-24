baad2f25c5009903dcae8b1781257eed788f5a8c
David Bucciarelli <dade916@gmail.com>
2015-02-24 11:21:41 +0100

    ParserLXS: now always using scene.camera.screenwindow at start and removing the property only after a window resize

--- samples/smallluxgpu4/smallluxgpu.cpp
+++ samples/smallluxgpu4/smallluxgpu.cpp
@@ -265,9 +265,6 @@ int main(int argc, char *argv[]) {
 			SLG_LOG("Scene: \n" << sceneProps);
 
 			renderConfigProps.Set(cmdLineProp);
-			// Delete scene.camera.screenwindow so window resize will
-			// automatically adjust the ratio
-			sceneProps.DeleteAll(sceneProps.GetAllNames("scene.camera.screenwindow"));
 
 			scene = new Scene(renderConfigProps.Get(Property("images.scale")(1.f)).Get<float>());
 			scene->Parse(sceneProps);
