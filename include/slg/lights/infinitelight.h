e441cc0ccb923995206dadc0ea44285dfc9ace6f
David Bucciarelli <dade916@gmail.com>
2015-02-24 12:40:12 +0100

    File splitting of all light related sources

---
+++ include/slg/lights/infinitelight.h
@@ -0,0 +1,69 @@
+/***************************************************************************
+ * Copyright 1998-2013 by authors (see AUTHORS.txt)                        *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ * Licensed under the Apache License, Version 2.0 (the "License");         *
+ * you may not use this file except in compliance with the License.        *
+ * You may obtain a copy of the License at                                 *
+ *                                                                         *
+ *     http://www.apache.org/licenses/LICENSE-2.0                          *
+ *                                                                         *
+ * Unless required by applicable law or agreed to in writing, software     *
+ * distributed under the License is distributed on an "AS IS" BASIS,       *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ * See the License for the specific language governing permissions and     *
+ * limitations under the License.                                          *
+ ***************************************************************************/
+
+#ifndef _SLG_INFINITELIGHT_H
+#define	_SLG_INFINITELIGHT_H
+
+#include "slg/lights/light.h"
+
+namespace slg {
+
+//------------------------------------------------------------------------------
+// InfiniteLight implementation
+//------------------------------------------------------------------------------
+
+class InfiniteLight : public EnvLightSource {
+public:
+	InfiniteLight();
+	virtual ~InfiniteLight();
+
+	virtual void Preprocess();
+	void GetPreprocessedData(const luxrays::Distribution2D **imageMapDistribution) const;
+
+	virtual LightSourceType GetType() const { return TYPE_IL; }
+	virtual float GetPower(const Scene &scene) const;
+
+	virtual luxrays::Spectrum Emit(const Scene &scene,
+		const float u0, const float u1, const float u2, const float u3, const float passThroughEvent,
+		luxrays::Point *pos, luxrays::Vector *dir,
+		float *emissionPdfW, float *directPdfA = NULL, float *cosThetaAtLight = NULL) const;
+
+    virtual luxrays::Spectrum Illuminate(const Scene &scene, const luxrays::Point &p,
+		const float u0, const float u1, const float passThroughEvent,
+        luxrays::Vector *dir, float *distance, float *directPdfW,
+		float *emissionPdfW = NULL, float *cosThetaAtLight = NULL) const;
+
+	virtual luxrays::Spectrum GetRadiance(const Scene &scene, const luxrays::Vector &dir,
+			float *directPdfA = NULL, float *emissionPdfW = NULL) const;
+
+	virtual void AddReferencedImageMaps(boost::unordered_set<const ImageMap *> &referencedImgMaps) const {
+		referencedImgMaps.insert(imageMap);
+	}
+
+	virtual luxrays::Properties ToProperties(const ImageMapCache &imgMapCache) const;
+
+	const ImageMap *imageMap;
+	UVMapping2D mapping;
+
+private:	
+	luxrays::Distribution2D *imageMapDistribution;
+};
+
+}
+
+#endif	/* _SLG_INFINITELIGHT_H */
