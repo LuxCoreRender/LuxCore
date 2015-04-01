66bbc57932e1a45383d38bd7168b504d2c0ff360
David Bucciarelli <dade916@gmail.com>
2015-04-01 17:52:10 +0200

    File splitting of more texture related code

---
+++ include/slg/textures/brick.h
@@ -0,0 +1,124 @@
+/***************************************************************************
+ * Copyright 1998-2013 by authors (see AUTHORS.txt)                        *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ * Licensed under the Apache License, Version 2.0 (the "License");         *
+ * you may not use this file except in compliance with the License.        *
+ * You may obtain a copy of the License at                                 *
+ *                                                                         *
+ *     http://www.apache.org/licenses/LICENSE-2.0                          *
+ *                                                                         *
+ * Unless required by applicable law or agreed to in writing, software     *
+ * distributed under the License is distributed on an "AS IS" BASIS,       *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ * See the License for the specific language governing permissions and     *
+ * limitations under the License.                                          *
+ ***************************************************************************/
+
+#ifndef _SLG_BRICKTEX_H
+#define	_SLG_BRICKTEX_H
+
+#include "slg/textures/texture.h"
+
+namespace slg {
+
+//------------------------------------------------------------------------------
+// Brick texture
+//------------------------------------------------------------------------------
+
+typedef enum {
+	FLEMISH, RUNNING, ENGLISH, HERRINGBONE, BASKET, KETTING
+} MasonryBond;
+
+class BrickTexture : public Texture {
+public:
+	BrickTexture(const TextureMapping3D *mp, const Texture *t1,
+			const Texture *t2, const Texture *t3,
+			float brickw, float brickh, float brickd, float mortar,
+			float r, float bev, const std::string &b);
+	virtual ~BrickTexture() { delete mapping; }
+
+	virtual TextureType GetType() const { return BRICK; }
+	virtual float GetFloatValue(const HitPoint &hitPoint) const;
+	virtual luxrays::Spectrum GetSpectrumValue(const HitPoint &hitPoint) const;
+	virtual float Y() const {
+		const float m = powf(luxrays::Clamp(1.f - mortarsize, 0.f, 1.f), 3);
+		return luxrays::Lerp(m, tex2->Y(), tex1->Y());
+	}
+	virtual float Filter() const {
+		const float m = powf(luxrays::Clamp(1.f - mortarsize, 0.f, 1.f), 3);
+		return luxrays::Lerp(m, tex2->Filter(), tex1->Filter());
+	}
+
+	virtual void AddReferencedTextures(boost::unordered_set<const Texture *> &referencedTexs) const {
+		Texture::AddReferencedTextures(referencedTexs);
+
+		tex1->AddReferencedTextures(referencedTexs);
+		tex2->AddReferencedTextures(referencedTexs);
+		tex3->AddReferencedTextures(referencedTexs);
+	}
+	virtual void AddReferencedImageMaps(boost::unordered_set<const ImageMap *> &referencedImgMaps) const {
+		tex1->AddReferencedImageMaps(referencedImgMaps);
+		tex2->AddReferencedImageMaps(referencedImgMaps);
+		tex3->AddReferencedImageMaps(referencedImgMaps);
+	}
+
+	virtual void UpdateTextureReferences(const Texture *oldTex, const Texture *newTex) {
+		if (tex1 == oldTex)
+			tex1 = newTex;
+		if (tex2 == oldTex)
+			tex2 = newTex;
+		if (tex3 == oldTex)
+			tex3 = newTex;
+	}
+
+	const TextureMapping3D *GetTextureMapping() const { return mapping; }
+	const Texture *GetTexture1() const { return tex1; }
+	const Texture *GetTexture2() const { return tex2; }
+	const Texture *GetTexture3() const { return tex3; }
+	MasonryBond GetBond() const { return bond; }
+	const luxrays::Point &GetOffset() const { return offset; }
+	float GetBrickWidth() const { return initialbrickwidth; }
+	float GetBrickHeight() const { return initialbrickheight; }
+	float GetBrickDepth() const { return initialbrickdepth; }
+	float GetMortarSize() const { return mortarsize; }
+	float GetProportion() const { return proportion; }
+	float GetInvProportion() const { return invproportion; }
+	float GetRun() const { return run; }
+	float GetMortarWidth() const { return mortarwidth; }
+	float GetMortarHeight() const { return mortarheight; }
+	float GetMortarDepth() const { return mortardepth; }
+	float GetBevelWidth() const { return bevelwidth; }
+	float GetBevelHeight() const { return bevelheight; }
+	float GetBevelDepth() const { return beveldepth; }
+	bool GetUseBevel() const { return usebevel; }
+
+	virtual luxrays::Properties ToProperties(const ImageMapCache &imgMapCache) const;
+
+private:
+	bool RunningAlternate(const luxrays::Point &p, luxrays::Point &i, luxrays::Point &b, int nWhole) const;
+	bool Basket(const luxrays::Point &p, luxrays::Point &i) const;
+	bool Herringbone(const luxrays::Point &p, luxrays::Point &i) const;
+	bool Running(const luxrays::Point &p, luxrays::Point &i, luxrays::Point &b) const;
+	bool English(const luxrays::Point &p, luxrays::Point &i, luxrays::Point &b) const;
+
+	const TextureMapping3D *mapping;
+	const Texture *tex1, *tex2, *tex3;
+
+	MasonryBond bond;
+	luxrays::Point offset;
+	float brickwidth, brickheight, brickdepth, mortarsize;
+	float proportion, invproportion, run;
+	float mortarwidth, mortarheight, mortardepth;
+	float bevelwidth, bevelheight, beveldepth;
+	bool usebevel;
+
+	// brickwidth, brickheight, brickdepth are modified by HERRINGBONE
+	// and BASKET brick types. I need to save the initial values here.
+	float initialbrickwidth, initialbrickheight, initialbrickdepth;
+};
+
+}
+
+#endif	/* _SLG_BRICKTEX_H */
---
+++ src/slg/textures/brick.cpp
@@ -0,0 +1,254 @@
+/***************************************************************************
+ * Copyright 1998-2013 by authors (see AUTHORS.txt)                        *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ * Licensed under the Apache License, Version 2.0 (the "License");         *
+ * you may not use this file except in compliance with the License.        *
+ * You may obtain a copy of the License at                                 *
+ *                                                                         *
+ *     http://www.apache.org/licenses/LICENSE-2.0                          *
+ *                                                                         *
+ * Unless required by applicable law or agreed to in writing, software     *
+ * distributed under the License is distributed on an "AS IS" BASIS,       *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ * See the License for the specific language governing permissions and     *
+ * limitations under the License.                                          *
+ ***************************************************************************/
+
+#include "slg/textures/brick.h"
+
+using namespace std;
+using namespace luxrays;
+using namespace slg;
+
+//------------------------------------------------------------------------------
+// Brick texture
+//------------------------------------------------------------------------------
+
+BrickTexture::BrickTexture(const TextureMapping3D *mp, const Texture *t1,
+		const Texture *t2, const Texture *t3,
+		float brickw, float brickh, float brickd, float mortar,
+		float r, float bev, const string &b) :
+		mapping(mp), tex1(t1), tex2(t2), tex3(t3),
+		brickwidth(brickw), brickheight(brickh), brickdepth(brickd), mortarsize(mortar),
+		run(r), initialbrickwidth(brickw), initialbrickheight(brickh), initialbrickdepth(brickd) {
+	if (b == "stacked") {
+		bond = RUNNING;
+		run = 0.f;
+	} else if (b == "flemish")
+		bond = FLEMISH;
+	else if (b == "english") {
+		bond = ENGLISH;
+		run = 0.25f;
+	} else if (b == "herringbone")
+		bond = HERRINGBONE;
+	else if (b == "basket")
+		bond = BASKET;
+	else if (b == "chain link") {
+		bond = KETTING;
+		run = 1.25f;
+		offset = Point(.25f, -1.f, 0.f);
+	} else {
+		bond = RUNNING;
+		offset = Point(0.f, -.5f , 0.f);
+	}
+	if (bond == HERRINGBONE || bond == BASKET) {
+		proportion = floorf(brickwidth / brickheight);
+		brickdepth = brickheight = brickwidth;
+		invproportion = 1.f / proportion;
+	}
+	mortarwidth = mortarsize / brickwidth;
+	mortarheight = mortarsize / brickheight;
+	mortardepth = mortarsize / brickdepth;
+	bevelwidth = bev / brickwidth;
+	bevelheight = bev / brickheight;
+	beveldepth = bev / brickdepth;
+	usebevel = bev > 0.f;
+}
+
+float BrickTexture::GetFloatValue(const HitPoint &hitPoint) const {
+	return GetSpectrumValue(hitPoint).Y();
+}
+
+bool BrickTexture::RunningAlternate(const Point &p, Point &i, Point &b,
+		int nWhole) const {
+	const float sub = nWhole + 0.5f;
+	const float rsub = ceilf(sub);
+	i.z = floorf(p.z);
+	b.x = (p.x + i.z * run) / sub;
+	b.y = (p.y + i.z * run) / sub;
+	i.x = floorf(b.x);
+	i.y = floorf(b.y);
+	b.x = (b.x - i.x) * sub;
+	b.y = (b.y - i.y) * sub;
+	b.z = (p.z - i.z) * sub;
+	i.x += floor(b.x) / rsub;
+	i.y += floor(b.y) / rsub;
+	b.x -= floor(b.x);
+	b.y -= floor(b.y);
+	return b.z > mortarheight && b.y > mortardepth &&
+		b.x > mortarwidth;
+}
+
+bool BrickTexture::Basket(const Point &p, Point &i) const {
+	i.x = floorf(p.x);
+	i.y = floorf(p.y);
+	float bx = p.x - i.x;
+	float by = p.y - i.y;
+	i.x += i.y - 2.f * floorf(0.5f * i.y);
+	const bool split = (i.x - 2.f * floor(0.5f * i.x)) < 1.f;
+	if (split) {
+		bx = fmodf(bx, invproportion);
+		i.x = floorf(proportion * p.x) * invproportion;
+	} else {
+		by = fmodf(by, invproportion);
+		i.y = floorf(proportion * p.y) * invproportion;
+	}
+	return by > mortardepth && bx > mortarwidth;
+}
+
+bool BrickTexture::Herringbone(const Point &p, Point &i) const {
+	i.y = floorf(proportion * p.y);
+	const float px = p.x + i.y * invproportion;
+	i.x = floorf(px);
+	float bx = 0.5f * px - floorf(px * 0.5f);
+	bx *= 2.f;
+	float by = proportion * p.y - floorf(proportion * p.y);
+	by *= invproportion;
+	if (bx > 1.f + invproportion) {
+		bx = proportion * (bx - 1.f);
+		i.y -= floorf(bx - 1.f);
+		bx -= floorf(bx);
+		bx *= invproportion;
+		by = 1.f;
+	} else if (bx > 1.f) {
+		bx = proportion * (bx - 1.f);
+		i.y -= floorf(bx - 1.f);
+		bx -= floorf(bx);
+		bx *= invproportion;
+	}
+	return by > mortarheight && bx > mortarwidth;
+}
+
+bool BrickTexture::Running(const Point &p, Point &i, Point &b) const {
+	i.z = floorf(p.z);
+	b.x = p.x + i.z * run;
+	b.y = p.y - i.z * run;
+	i.x = floorf(b.x);
+	i.y = floorf(b.y);
+	b.z = p.z - i.z;
+	b.x -= i.x;
+	b.y -= i.y;
+	return b.z > mortarheight && b.y > mortardepth &&
+		b.x > mortarwidth;
+}
+
+bool BrickTexture::English(const Point &p, Point &i, Point &b) const {
+	i.z = floorf(p.z);
+	b.x = p.x + i.z * run;
+	b.y = p.y - i.z * run;
+	i.x = floorf(b.x);
+	i.y = floorf(b.y);
+	b.z = p.z - i.z;
+	const float divider = floorf(fmodf(fabsf(i.z), 2.f)) + 1.f;
+	b.x = (divider * b.x - floorf(divider * b.x)) / divider;
+	b.y = (divider * b.y - floorf(divider * b.y)) / divider;
+	return b.z > mortarheight && b.y > mortardepth &&
+		b.x > mortarwidth;
+}
+
+Spectrum BrickTexture::GetSpectrumValue(const HitPoint &hitPoint) const {
+#define BRICK_EPSILON 1e-3f
+	const Point P(mapping->Map(hitPoint));
+
+	const float offs = BRICK_EPSILON + mortarsize;
+	Point bP(P + Point(offs, offs, offs));
+
+	// Normalize coordinates according to brick dimensions
+	bP.x /= brickwidth;
+	bP.y /= brickdepth;
+	bP.z /= brickheight;
+
+	bP += offset;
+
+	Point brickIndex;
+	Point bevel;
+	bool b;
+	switch (bond) {
+		case FLEMISH:
+			b = RunningAlternate(bP, brickIndex, bevel, 1);
+			break;
+		case RUNNING:
+			b = Running(bP, brickIndex, bevel);
+			break;
+		case ENGLISH:
+			b = English(bP, brickIndex, bevel);
+			break;
+		case HERRINGBONE:
+			b = Herringbone(bP, brickIndex);
+			break;
+		case BASKET:
+			b = Basket(bP, brickIndex);
+			break;
+		case KETTING:
+			b = RunningAlternate(bP, brickIndex, bevel, 2);
+			break; 
+		default:
+			b = true;
+			break;
+	}
+
+	if (b) {
+		// Brick texture * Modulation texture
+		return tex1->GetSpectrumValue(hitPoint) * tex3->GetSpectrumValue(hitPoint);
+	} else {
+		// Mortar texture
+		return tex2->GetSpectrumValue(hitPoint);
+	}
+#undef BRICK_EPSILON
+}
+
+Properties BrickTexture::ToProperties(const ImageMapCache &imgMapCache) const {
+	Properties props;
+
+	const string name = GetName();
+	props.Set(Property("scene.textures." + name + ".type")("brick"));
+	props.Set(Property("scene.textures." + name + ".bricktex")(tex1->GetName()));
+	props.Set(Property("scene.textures." + name + ".mortartex")(tex2->GetName()));
+	props.Set(Property("scene.textures." + name + ".brickmodtex")(tex3->GetName()));
+	props.Set(Property("scene.textures." + name + ".brickwidth")(brickwidth));
+	props.Set(Property("scene.textures." + name + ".brickheight")(brickheight));
+	props.Set(Property("scene.textures." + name + ".brickdepth")(brickdepth));
+	props.Set(Property("scene.textures." + name + ".mortarsize")(mortarsize));
+	props.Set(Property("scene.textures." + name + ".brickrun")(run));
+	props.Set(Property("scene.textures." + name + ".brickbevel")(bevelwidth * brickwidth));
+
+	string brickBondValue;
+	switch (bond) {
+		case FLEMISH:
+			brickBondValue = "flemish";
+			break;
+		case ENGLISH:
+			brickBondValue = "english";
+			break;
+		case HERRINGBONE:
+			brickBondValue = "herringbone";
+			break;
+		case BASKET:
+			brickBondValue = "basket";
+			break;
+		case KETTING:
+			brickBondValue = "chain link";
+			break;
+		default:
+		case RUNNING:
+			brickBondValue = "stacked";
+			break;
+	}
+	props.Set(Property("scene.textures." + name + ".brickbond")(brickBondValue));
+
+	props.Set(mapping->ToProperties("scene.textures." + name + ".mapping"));
+
+	return props;
+}
