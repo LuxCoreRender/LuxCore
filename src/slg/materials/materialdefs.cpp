d1502d777cb3f1c509e822523693fd064ffe1244
David Bucciarelli <dade916@gmail.com>
2015-04-10 15:51:51 +0200

    File splitting of material related code

---
+++ src/slg/materials/materialdefs.cpp
@@ -0,0 +1,102 @@
+/***************************************************************************
+ * Copyright 1998-2013 by authors (see AUTHORS.txt)                        *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ * Licensed under the Apache License, Version 2.0 (the "License");         *
+ * you may not use this file except in compliance with the License.        *
+ * You may obtain a copy of the License at                                 *
+ *                                                                         *
+ *     http://www.apache.org/licenses/LICENSE-2.0                          *
+ *                                                                         *
+ * Unless required by applicable law or agreed to in writing, software     *
+ * distributed under the License is distributed on an "AS IS" BASIS,       *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ * See the License for the specific language governing permissions and     *
+ * limitations under the License.                                          *
+ ***************************************************************************/
+
+#include <boost/lexical_cast.hpp>
+
+#include "slg/materials/materialdefs.h"
+
+using namespace std;
+using namespace luxrays;
+using namespace slg;
+
+//------------------------------------------------------------------------------
+// MaterialDefinitions
+//------------------------------------------------------------------------------
+
+MaterialDefinitions::MaterialDefinitions() { }
+
+MaterialDefinitions::~MaterialDefinitions() {
+	BOOST_FOREACH(Material *m, mats)
+		delete m;
+}
+
+void MaterialDefinitions::DefineMaterial(const string &name, Material *newMat) {
+	if (IsMaterialDefined(name)) {
+		Material *oldMat = GetMaterial(name);
+
+		// Update name/material definition
+		const u_int index = GetMaterialIndex(name);
+		mats[index] = newMat;
+		matsByName.erase(name);
+		matsByName.insert(make_pair(name, newMat));
+
+		// Update all possible references to old material with the new one
+		BOOST_FOREACH(Material *mat, mats)
+			mat->UpdateMaterialReferences(oldMat, newMat);
+
+		// Delete old material
+		delete oldMat;
+	} else {
+		// Add the new material
+		mats.push_back(newMat);
+		matsByName.insert(make_pair(name, newMat));
+	}
+}
+
+void MaterialDefinitions::UpdateTextureReferences(const Texture *oldTex, const Texture *newTex) {
+	BOOST_FOREACH(Material *mat, mats)
+		mat->UpdateTextureReferences(oldTex, newTex);
+}
+
+Material *MaterialDefinitions::GetMaterial(const string &name) {
+	// Check if the material has been already defined
+	boost::unordered_map<string, Material *>::const_iterator it = matsByName.find(name);
+
+	if (it == matsByName.end())
+		throw runtime_error("Reference to an undefined material: " + name);
+	else
+		return it->second;
+}
+
+u_int MaterialDefinitions::GetMaterialIndex(const string &name) {
+	return GetMaterialIndex(GetMaterial(name));
+}
+
+u_int MaterialDefinitions::GetMaterialIndex(const Material *m) const {
+	for (u_int i = 0; i < mats.size(); ++i) {
+		if (m == mats[i])
+			return i;
+	}
+
+	throw runtime_error("Reference to an undefined material: " + boost::lexical_cast<string>(m));
+}
+
+vector<string> MaterialDefinitions::GetMaterialNames() const {
+	vector<string> names;
+	names.reserve(mats.size());
+	for (boost::unordered_map<string, Material *>::const_iterator it = matsByName.begin(); it != matsByName.end(); ++it)
+		names.push_back(it->first);
+
+	return names;
+}
+
+void MaterialDefinitions::DeleteMaterial(const string &name) {
+	const u_int index = GetMaterialIndex(name);
+	mats.erase(mats.begin() + index);
+	matsByName.erase(name);
+}
