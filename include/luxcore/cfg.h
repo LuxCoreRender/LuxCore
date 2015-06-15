cc154bd99e8bc3ba34c703a5697ef6c20f6eb281
Jean-Philippe Grimaldi <jeanphi@via.ecp.fr>
2015-06-15 22:17:17 +0200

    Update version to v1.5RC1
    Jeanphi

--- include/luxcore/cfg.h
+++ include/luxcore/cfg.h
@@ -22,6 +22,6 @@
 // The configured options and settings for SmallLuxGPU
 
 #define LUXCORE_VERSION_MAJOR "1"
-#define LUXCORE_VERSION_MINOR "5dev"
+#define LUXCORE_VERSION_MINOR "5RC1"
 
 #endif	/* _LUXCORE_CFG_H */
--- include/luxrays/cfg.h
+++ include/luxrays/cfg.h
@@ -22,6 +22,6 @@
 // The configured options and settings for LuxRays
 
 #define LUXRAYS_VERSION_MAJOR "1"
-#define LUXRAYS_VERSION_MINOR "5dev"
+#define LUXRAYS_VERSION_MINOR "5RC1"
 
 #endif	/* _LUXRAYS_CFG_H */
