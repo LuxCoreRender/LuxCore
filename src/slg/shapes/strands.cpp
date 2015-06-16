9089a84916577ced7cb8af7b26e59e33103d2bb0
David Bucciarelli <dade916@gmail.com>
2015-06-16 11:09:30 +0200

    Fixed strands shape normal orientation

--- src/slg/shapes/strands.cpp
+++ src/slg/shapes/strands.cpp
@@ -661,14 +661,14 @@ void StrendsShape::TessellateSolid(const Scene *scene,
 			const u_int i3 = (j == solidSideCount - 1) ? (index + solidSideCount) : (index + j + solidSideCount  + 1);
 
 			// First triangle
-			meshTris.push_back(Triangle(i0, i1, i2));
+			meshTris.push_back(Triangle(i0, i2, i1));
 			const Normal n0 = Normal(Cross(meshVerts[i2] - meshVerts[i0], meshVerts[i1] - meshVerts[i0]));
 			meshNorms[i0] += n0;
 			meshNorms[i1] += n0;
 			meshNorms[i2] += n0;
 
 			// Second triangle
-			meshTris.push_back(Triangle(i1, i3, i2));
+			meshTris.push_back(Triangle(i1, i2, i3));
 			const Normal n1 = Normal(Cross(meshVerts[i2] - meshVerts[i0], meshVerts[i3] - meshVerts[i0]));
 			meshNorms[i1] += n1;
 			meshNorms[i3] += n1;
@@ -700,7 +700,7 @@ void StrendsShape::TessellateSolid(const Scene *scene,
 			const u_int i0 = offset + j;
 			const u_int i1 = (j == solidSideCount - 1) ? offset : (offset + j + 1);
 
-			meshTris.push_back(Triangle(i0, i1, i3));
+			meshTris.push_back(Triangle(i1, i0, i3));
 		}
 	}
 
@@ -728,7 +728,7 @@ void StrendsShape::TessellateSolid(const Scene *scene,
 			const u_int i0 = offset + j;
 			const u_int i1 = (j == solidSideCount - 1) ? offset : (offset + j + 1);
 
-			meshTris.push_back(Triangle(i1, i0, i3));
+			meshTris.push_back(Triangle(i0, i1, i3));
 		}
 	}
 }
