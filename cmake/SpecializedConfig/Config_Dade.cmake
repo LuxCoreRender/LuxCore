cd022232db5a7ed5dcec0b29b687bfe0f594342d
David Bucciarelli <dade916@gmail.com>
2015-07-08 11:20:45 +0200

    A couple of Config_Dade.cmake edits

--- cmake/SpecializedConfig/Config_Dade.cmake
+++ cmake/SpecializedConfig/Config_Dade.cmake
@@ -10,21 +10,21 @@
 MESSAGE(STATUS "Using Dade's Linux Configuration settings")
 
 # To compile with a specific Python version
-#set(Python_ADDITIONAL_VERSIONS "2.6")
+#SET(Python_ADDITIONAL_VERSIONS "2.6")
 
 # To pick a specific minimum Python version
-#set(PythonLibs_FIND_VERSION 3)
+#SET(PythonLibs_FIND_VERSION 3)
 
-#set(BOOST_SEARCH_PATH          "/home/david/projects/luxrender-dev/boost_1_53_0")
-set(OPENIMAGEIO_ROOT_DIR        "/home/david/projects/luxrender-dev/oiio-RB-1.3/dist/linux64")
-set(OPENEXR_ROOT                "/usr/local")
-set(EMBREE_SEARCH_PATH           "/home/david/src/embree-bin-2.4_linux")
+#SET(BOOST_SEARCH_PATH		"/home/david/projects/luxrender-dev/boost_1_53_0")
+#SET(OPENIMAGEIO_ROOT_DIR	"/home/david/projects/luxrender-dev/oiio-RB-1.3/dist/linux64")
+#SET(OPENEXR_ROOT			"/usr/local")
+SET(EMBREE_SEARCH_PATH		"/home/david/src/embree-bin-2.4_linux")
 
-set(OPENCL_SEARCH_PATH        "$ENV{AMDAPPSDKROOT}")
-set(OPENCL_INCLUDEPATH        "${OPENCL_SEARCH_PATH}/include")
-#set(OPENCL_LIBRARYDIR         "${OPENCL_SEARCH_PATH}/lib/x86_64")
+SET(OPENCL_SEARCH_PATH	$ENV{AMDAPPSDKROOT}")
+SET(OPENCL_INCLUDEPATH	"${OPENCL_SEARCH_PATH}/include")
+#SET(OPENCL_LIBRARYDIR	"${OPENCL_SEARCH_PATH}/lib/x86_64")
 
-#set(LUXRAYS_DISABLE_OPENCL TRUE)
+#SET(LUXRAYS_DISABLE_OPENCL TRUE)
 
-#set(CMAKE_BUILD_TYPE "Debug")
-set(CMAKE_BUILD_TYPE "Release")
+#SET(CMAKE_BUILD_TYPE "Debug")
+SET(CMAKE_BUILD_TYPE "Release")
