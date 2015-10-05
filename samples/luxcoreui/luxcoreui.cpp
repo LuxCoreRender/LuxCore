1c1188f26588be05f4a38f7a0e07879ac2a98937
David Bucciarelli <dade916@gmail.com>
2015-10-05 18:23:23 +0200

    Initial work on luxcoreconsole and luxcoreui

---
+++ samples/luxcoreui/luxcoreui.cpp
@@ -0,0 +1,85 @@
+/***************************************************************************
+ * Copyright 1998-2015 by authors (see AUTHORS.txt)                        *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ * Licensed under the Apache License, Version 2.0 (the "License");         *
+ * you may not use this file except in compliance with the License.        *
+ * You may obtain a copy of the License at                                 *
+ *                                                                         *
+ *     http://www.apache.org/licenses/LICENSE-2.0                          *
+ *                                                                         *
+ * Unless required by applicable law or agreed to in writing, software     *
+ * distributed under the License is distributed on an "AS IS" BASIS,       *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ * See the License for the specific language governing permissions and     *
+ * limitations under the License.                                          *
+ ***************************************************************************/
+
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+
+#include "luxcore/luxcore.h"
+
+using namespace std;
+using namespace luxrays;
+using namespace luxcore;
+
+static void GLFWErrorCallback(int error, const char *description) {
+	cout <<
+			"GLFW Error: " << error << "\n"
+			"Description: " << description << "\n";
+}
+
+static void KeyCallBack(GLFWwindow *window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int main(int argc, char *argv[]) {
+	glfwSetErrorCallback(GLFWErrorCallback);
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+
+	GLFWwindow *window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, KeyCallBack);
+
+	while (!glfwWindowShouldClose(window)) {
+		float ratio;
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+
+		ratio = width / (float) height;
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+
+		glBegin(GL_TRIANGLES);
+		glColor3f(1.f, 0.f, 0.f);
+		glVertex3f(-0.6f, -0.4f, 0.f);
+		glColor3f(0.f, 1.f, 0.f);
+		glVertex3f(0.6f, -0.4f, 0.f);
+		glColor3f(0.f, 0.f, 1.f);
+		glVertex3f(0.f, 0.6f, 0.f);
+		glEnd();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	exit(EXIT_SUCCESS);
+}
