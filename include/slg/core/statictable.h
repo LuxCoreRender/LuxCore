58b90968dfbe7ccb4f6e4ac3ffd51e2a46c5c52e
David Bucciarelli <dade916@gmail.com>
2015-10-18 17:08:07 +0200

    Now String2SamplerType() and SamplerType2String() use StaticTable too

--- include/slg/core/statictable.h
+++ include/slg/core/statictable.h
@@ -23,6 +23,7 @@
 #include <boost/unordered_map.hpp>
 
 #include "luxrays/luxrays.h"
+#include "luxrays/core/utils.h"
 
 namespace slg {
 
@@ -31,26 +32,28 @@ public:
 	StaticTable() { }
 	~StaticTable() { }
 
-	T Get(const K &name) const {
+	bool Get(const K &key, T &val) const {
 		const boost::unordered_map<K, T> &table = GetTable();
 
-		typename boost::unordered_map<K, T>::const_iterator it = table.find(name);
+		typename boost::unordered_map<K, T>::const_iterator it = table.find(key);
 		if (it == table.end())
-			return NULL;
-		else
-			return it->second;
+			return false;
+		else {
+			val = it->second;
+			return true;
+		}
 	}
 
 	class RegisterTableValue {
 	public:
-		RegisterTableValue(const K &name, T val) {
+		RegisterTableValue(const K &key, const T &val) {
 			boost::unordered_map<K, T> &table = GetTable();
 
-			typename boost::unordered_map<K, T>::const_iterator it = table.find(name);
+			typename boost::unordered_map<K, T>::const_iterator it = table.find(key);
 			if (it == table.end())
-				table[name] = val;
+				table[key] = val;
 			else
-				throw std::runtime_error("Already registered name in StaticTable::RegisterTableValue::RegisterTableValue(): " + name);
+				throw std::runtime_error("Already registered key in StaticTable::RegisterTableValue::RegisterTableValue(): " + luxrays::ToString(key));
 		}
 		virtual ~RegisterTableValue() { }
 	};
@@ -69,17 +72,17 @@ private:
 #define STATICTABLE_NAME(F) F ## _StaticTable
 
 // Use STATICTABLE_DECLARE_DECLARATION inside the base class holding the table
-#define STATICTABLE_DECLARE_DECLARATION(F) static StaticTable<std::string, F ## StaticTableType> STATICTABLE_NAME(F)
+#define STATICTABLE_DECLARE_DECLARATION(K, F) static StaticTable<K, F ## StaticTableType> STATICTABLE_NAME(F)
 // Use STATICTABLE_DECLARATION inside the base class .cpp
-#define STATICTABLE_DECLARATION(C, F) StaticTable<std::string, C::F ## StaticTableType> C::STATICTABLE_NAME(F)
+#define STATICTABLE_DECLARATION(C, K, F) StaticTable<K, C::F ## StaticTableType> C::STATICTABLE_NAME(F)
 
 // Use STATICTABLE_DECLARE_REGISTRATION() inside the class declaration to register
-#define STATICTABLE_DECLARE_REGISTRATION(C, F) static StaticTable<std::string, F ## StaticTableType>::RegisterTableValue C ## F ## _StaticTableRegisterTableValue
+#define STATICTABLE_DECLARE_REGISTRATION(C, K, F) static StaticTable<K, F ## StaticTableType>::RegisterTableValue C ## F ## _StaticTableRegisterTableValue
 // Use STATICTABLE_REGISTER() to register a class
 // NOTE: you have to place all STATICTABLE_REGISTER() in the same .cpp file of the
 // main base class (i.e. the one holding the StaticTable) because order of static
 // field initialization is otherwise undefined.
-#define STATICTABLE_REGISTER(R, C, N, F) StaticTable<std::string, R::F ## StaticTableType>::RegisterTableValue R::C ## F ## _StaticTableRegisterTableValue(N, C::F)
+#define STATICTABLE_REGISTER(R, C, N, K, F) StaticTable<K, R::F ## StaticTableType>::RegisterTableValue R::C ## F ## _StaticTableRegisterTableValue(N, C::F)
 
 }
 
