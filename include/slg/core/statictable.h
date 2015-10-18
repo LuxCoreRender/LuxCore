bd96061f89d205387b3dfd5d1f2b625638af55d9
David Bucciarelli <dade916@gmail.com>
2015-10-18 16:11:24 +0200

    StaticTable is now a template with 2 parameters (one also fore keys)

--- include/slg/core/statictable.h
+++ include/slg/core/statictable.h
@@ -26,15 +26,15 @@
 
 namespace slg {
 
-template <class T> class StaticTable {
+template <class K, class T> class StaticTable {
 public:
 	StaticTable() { }
 	~StaticTable() { }
 
-	T Get(const std::string &name) const {
-		const boost::unordered_map<std::string, T> &table = GetTable();
+	T Get(const K &name) const {
+		const boost::unordered_map<K, T> &table = GetTable();
 
-		typename boost::unordered_map<std::string, T>::const_iterator it = table.find(name);
+		typename boost::unordered_map<K, T>::const_iterator it = table.find(name);
 		if (it == table.end())
 			return NULL;
 		else
@@ -43,10 +43,10 @@ public:
 
 	class RegisterTableValue {
 	public:
-		RegisterTableValue(const std::string &name, T val) {
-			boost::unordered_map<std::string, T> &table = GetTable();
+		RegisterTableValue(const K &name, T val) {
+			boost::unordered_map<K, T> &table = GetTable();
 
-			typename boost::unordered_map<std::string, T>::const_iterator it = table.find(name);
+			typename boost::unordered_map<K, T>::const_iterator it = table.find(name);
 			if (it == table.end())
 				table[name] = val;
 			else
@@ -56,11 +56,11 @@ public:
 	};
 
 private:
-	__attribute__ ((visibility ("default"))) static boost::unordered_map<std::string, T> &GetTable() {
+	static boost::unordered_map<K, T> &GetTable() {
 		// Using
-		static boost::unordered_map<std::string, T> *table = new boost::unordered_map<std::string, T>();
+		static boost::unordered_map<K, T> *table = new boost::unordered_map<K, T>();
 		// instead of:
-		//static boost::unordered_map<std::string, T> table;
+		//static boost::unordered_map<K, T> table;
 		// to avoid possible allocation on thread heap.
 		return *table;
 	}
@@ -69,17 +69,17 @@ private:
 #define STATICTABLE_NAME(F) F ## _StaticTable
 
 // Use STATICTABLE_DECLARE_DECLARATION inside the base class holding the table
-#define STATICTABLE_DECLARE_DECLARATION(F) static StaticTable<F ## StaticTableType> STATICTABLE_NAME(F)
+#define STATICTABLE_DECLARE_DECLARATION(F) static StaticTable<std::string, F ## StaticTableType> STATICTABLE_NAME(F)
 // Use STATICTABLE_DECLARATION inside the base class .cpp
-#define STATICTABLE_DECLARATION(C, F) StaticTable<C::F ## StaticTableType> C::STATICTABLE_NAME(F)
+#define STATICTABLE_DECLARATION(C, F) StaticTable<std::string, C::F ## StaticTableType> C::STATICTABLE_NAME(F)
 
 // Use STATICTABLE_DECLARE_REGISTRATION() inside the class declaration to register
-#define STATICTABLE_DECLARE_REGISTRATION(C, F) static StaticTable<F ## StaticTableType>::RegisterTableValue C ## F ## _StaticTableRegisterTableValue
+#define STATICTABLE_DECLARE_REGISTRATION(C, F) static StaticTable<std::string, F ## StaticTableType>::RegisterTableValue C ## F ## _StaticTableRegisterTableValue
 // Use STATICTABLE_REGISTER() to register a class
 // NOTE: you have to place all STATICTABLE_REGISTER() in the same .cpp file of the
 // main base class (i.e. the one holding the StaticTable) because order of static
 // field initialization is otherwise undefined.
-#define STATICTABLE_REGISTER(R, C, N, F) StaticTable<R::F ## StaticTableType>::RegisterTableValue R::C ## F ## _StaticTableRegisterTableValue(N, C::F)
+#define STATICTABLE_REGISTER(R, C, N, F) StaticTable<std::string, R::F ## StaticTableType>::RegisterTableValue R::C ## F ## _StaticTableRegisterTableValue(N, C::F)
 
 }
 
