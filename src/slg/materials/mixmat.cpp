9e3d242dc19839a54a18cd023767b25e264292f8
David Bucciarelli <dade916@gmail.com>
2015-12-30 10:30:26 +0100

    Added the support for material transparency property to Mix material code

--- src/slg/materials/mixmat.cpp
+++ src/slg/materials/mixmat.cpp
@@ -94,13 +94,17 @@ const Volume *MixMaterial::GetExteriorVolume(const HitPoint &hitPoint,
 
 Spectrum MixMaterial::GetPassThroughTransparency(const HitPoint &hitPoint,
 		const Vector &localFixedDir, const float passThroughEvent) const {
-	const float weight2 = Clamp(mixFactor->GetFloatValue(hitPoint), 0.f, 1.f);
-	const float weight1 = 1.f - weight2;
+	if (transparencyTex)
+		return transparencyTex->GetSpectrumValue(hitPoint);
+	else {
+		const float weight2 = Clamp(mixFactor->GetFloatValue(hitPoint), 0.f, 1.f);
+		const float weight1 = 1.f - weight2;
 
-	if (passThroughEvent < weight1)
-		return matA->GetPassThroughTransparency(hitPoint, localFixedDir, passThroughEvent / weight1);
-	else
-		return matB->GetPassThroughTransparency(hitPoint, localFixedDir, (passThroughEvent - weight1) / weight2);
+		if (passThroughEvent < weight1)
+			return matA->GetPassThroughTransparency(hitPoint, localFixedDir, passThroughEvent / weight1);
+		else
+			return matB->GetPassThroughTransparency(hitPoint, localFixedDir, (passThroughEvent - weight1) / weight2);
+	}
 }
 
 float MixMaterial::GetEmittedRadianceY() const {
