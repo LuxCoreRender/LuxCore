dee9f7f2d319bd4ef906cdd7bcf6134e5636fb7e
David Bucciarelli <dade916@gmail.com>
2016-01-22 09:32:59 +0100

    Fixed work size in Reinhard02ToneMap and AutoLinearToneMap

--- src/slg/film/imagepipeline/plugins/tonemaps/reinhard02.cpp
+++ src/slg/film/imagepipeline/plugins/tonemaps/reinhard02.cpp
@@ -126,7 +126,7 @@ void Reinhard02ToneMap::Apply(Film &film, const u_int index) {
 #if !defined(LUXRAYS_DISABLE_OPENCL)
 void Reinhard02ToneMap::ApplyOCL(Film &film, const u_int index) {
 	const u_int pixelCount = film.GetWidth() * film.GetHeight();
-	const u_int workSize = RoundUp(pixelCount, 128u) / 2;
+	const u_int workSize = RoundUp((pixelCount + 1) / 2, 64u);
 
 	if (!applyKernel) {
 		// Allocate buffers
