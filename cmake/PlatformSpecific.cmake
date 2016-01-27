eaaa2dd91d7e6212b0d5fe51e9c1075e774051a4
Jens Verwiebe <info@jensverwiebe.de>
2016-01-27 16:11:45 +0100

    Linux: start using an export map for preventing symbol clashes, safer across distibutions

--- cmake/PlatformSpecific.cmake
+++ cmake/PlatformSpecific.cmake
@@ -179,6 +179,11 @@ IF(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
   
 ENDIF()
 
+IF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+	set(CMAKE_EXE_LINKER_FLAGS -Wl,--version-script='${CMAKE_SOURCE_DIR}/include/luxrays/utils/linux_locals.map')
+	set(CMAKE_SHARED_LINKER_FLAGS -Wl,--version-script='${CMAKE_SOURCE_DIR}/include/luxrays/utils/linux_locals.map')
+	set(CMAKE_MODULE_LINKER_FLAGS -Wl,--version-script='${CMAKE_SOURCE_DIR}/include/luxrays/utils/linux_locals.map')
+ENDIF()
 
 # Setting Universal Binary Properties, only for Mac OS X
 #  generate with xcode/crosscompile, setting: ( darwin - 10.6 - gcc - g++ - MacOSX10.6.sdk - Find from root, then native system )
