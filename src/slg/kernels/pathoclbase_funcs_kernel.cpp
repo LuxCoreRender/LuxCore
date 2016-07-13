6b3edab26057144a584a66d50db3ecf497e7ae6a
Jens Verwiebe <info@jensverwiebe.de>
2016-07-13 20:42:22 +0200

    Fix forgotten typo

--- src/slg/kernels/pathoclbase_funcs_kernel.cpp
+++ src/slg/kernels/pathoclbase_funcs_kernel.cpp
@@ -1,7 +1,7 @@
 #include <string>
 namespace slg { namespace ocl {
 std::string KernelSource_pathoclbase_funcs = 
-"#line 2 \"patchoclbase_funcs.cl\"\n"
+"#line 2 \"pathoclbase_funcs.cl\"\n"
 "\n"
 "/***************************************************************************\n"
 " * Copyright 1998-2015 by authors (see AUTHORS.txt)                        *\n"
