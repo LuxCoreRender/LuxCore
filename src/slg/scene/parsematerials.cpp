339ffa4c5bac3abf86b2d67f3082432935a04518
Jens Verwiebe <info@jensverwiebe.de>
2016-09-07 14:57:21 +0200

    Also give light/emission power a proper default

--- src/slg/scene/parsematerials.cpp
+++ src/slg/scene/parsematerials.cpp
@@ -413,7 +413,7 @@ Material *Scene::CreateMaterial(const u_int defaultMatID, const string &matName,
 	mat->SetBumpSampleDistance(bumpSampleDistance);
 
 	mat->SetEmittedGain(props.Get(Property(propName + ".emission.gain")(Spectrum(1.f))).Get<Spectrum>());
-	mat->SetEmittedPower(Max(0.f, props.Get(Property(propName + ".emission.power")(0.f)).Get<float>()));
+	mat->SetEmittedPower(Max(0.f, props.Get(Property(propName + ".emission.power")(100.f)).Get<float>()));
 	mat->SetEmittedEfficency(Max(0.f, props.Get(Property(propName + ".emission.efficency")(17.f)).Get<float>()));
 	mat->SetEmittedSamples(Max(-1, props.Get(Property(propName + ".emission.samples")(-1)).Get<int>()));
 	mat->SetLightID(props.Get(Property(propName + ".emission.id")(0u)).Get<u_int>());
