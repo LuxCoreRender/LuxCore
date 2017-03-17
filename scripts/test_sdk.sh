cb4600dafbd173043a65f4a0eb15d97dae1c4fc3
David Bucciarelli <dade916@gmail.com>
2017-03-17 15:33:11 +0100

    Added serialization to the list of Boost components used

--- scripts/test_sdk.sh
+++ scripts/test_sdk.sh
@@ -1,23 +1,15 @@
 #!/bin/sh
 
+# Use https://bitbucket.org/luxrender/linux to build LuxCore binaries and libraries
+
 rm -rf test-sdk
 mkdir test-sdk
 
+echo "================ Copy Scenes ================"
 cp -r scenes test-sdk
+
+echo "================ Copy LuxCore DLL ================"
 mkdir test-sdk/lib
-cd test-sdk/lib
-
-echo "================ Merge everything in a single library ================"
-echo "create libluxcore.a
-addlib ../../lib/libluxrays.a
-addlib ../../lib/libslg-core.a
-addlib ../../lib/libslg-film.a
-addlib ../../lib/libslg-kernels.a
-addlib ../../lib/libluxcore.a
-save
-end" | ar -M
-
-cd ../..
 
 echo "================ Copy headers ================"
 cp -v --parents include/luxcore/cfg.h test-sdk
@@ -48,5 +40,3 @@ echo "================ Copy cmake files ================"
 cp -v sdk/CMakeLists.txt test-sdk
 mkdir test-sdk/cmake
 cp -v cmake/Packages/FindOpenCL.cmake test-sdk/cmake
-
-#/usr/bin/c++  -fPIC -mtune=generic -std=c++11 -static-libgcc -mmmx -msse -msse2 -O3 -pipe -mfpmath=sse  -fvisibility-inlines-hidden -ftree-vectorize -fno-math-errno -fno-signed-zeros -fno-trapping-math -fassociative-math -fno-rounding-math -fno-signaling-nans -fcx-limited-range -DBOOST_DISABLE_ASSERTS -floop-interchange -floop-strip-mine -floop-block -fsee -ftree-loop-linear -ftree-loop-distribution -ftree-loop-im -fivopts -ftracer -fomit-frame-pointer -DHAVE_PTHREAD_H -fPIC -fno-stack-protector -pthread -lrt  -Wall -Wno-long-long -pedantic -msse -msse2 -msse3 -mssse3 -fPIC -fopenmp -DNDEBUG -O3 -ftree-vectorize -funroll-loops -fvariable-expansion-in-unroller -Wl,--version-script='/tmp/luxbuild/luxrays/cmake/exportmaps/linux_symbol_exports.map' -shared -Wl,-soname,luxcore.so -o luxcore.so -Wl,--whole-archive libluxcore.a -Wl,--no-whole-archive libslg-core.a libslg-film.a libslg-kernels.a libluxrays.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libembree.so.2 /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libboost_thread.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libboost_program_options.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libboost_filesystem.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libboost_serialization.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libboost_iostreams.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libboost_regex.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libboost_system.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libboost_python.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libboost_chrono.a -lpthread /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libtiff.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libIex.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libIlmImf.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libHalf.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libImath.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libIlmThread.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libpng.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libz.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libjpeg.a /home/david/projects/luxrender-dev/linux/target-64-sse2/lib/libOpenImageIO.a -lGL -lOpenCL -Wl,-rpath,"\$ORIGIN"
