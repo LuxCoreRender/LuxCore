7122d1a89b30c90064f8d1ee110e0bc823226e12
David Bucciarelli <dade916@gmail.com>
2018-02-08 12:57:07 +0100

    Changed version to v2.0alpha4

--- include/luxcore/cfg.h
+++ include/luxcore/cfg.h
@@ -22,6 +22,6 @@
 // The configured options and settings for LuxCore
 
 #define LUXCORE_VERSION_MAJOR "2"
-#define LUXCORE_VERSION_MINOR "0alpha3"
+#define LUXCORE_VERSION_MINOR "0alpha4"
 
 #endif	/* _LUXCORE_CFG_H */
