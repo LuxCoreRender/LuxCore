eb52786413aebb5fbf6e134d448596acfc755929
David Bucciarelli <dade916@gmail.com>
2018-03-14 15:48:58 +0100

    .py file are now generated from .ui files only if needed

--- src/pyluxcoretools/pyluxcoretools/pyluxcorenetnode/mainwindow.py
+++ src/pyluxcoretools/pyluxcoretools/pyluxcorenetnode/mainwindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/david/projects/luxcorerender/LuxCore/src/pyluxcoretools/pyluxcoretools/pyluxcorenetnode/mainwindow.ui'
 #
-# Created: Wed Mar 14 15:18:28 2018
+# Created: Wed Mar 14 15:25:39 2018
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
