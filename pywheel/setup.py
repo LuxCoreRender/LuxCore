4e08106b6ac18eb3833cd21a9808ef5d49f4216e
David Bucciarelli <dade916@gmail.com>
2018-04-02 12:13:24 +0200

    Updated release to v2.0beta1 in pywheel/setup.py

--- pywheel/setup.py
+++ pywheel/setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.0a4.dev1',  # Required
+    version='2.0beta1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
