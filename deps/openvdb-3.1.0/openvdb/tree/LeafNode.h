b8b42171e5d38a561543dbcc94170212cf2e848b
David Bucciarelli <dade916@gmail.com>
2018-04-09 18:23:06 +0200

    Fixed compiation on Windows

--- deps/openvdb-3.1.0/openvdb/tree/LeafNode.h
+++ deps/openvdb-3.1.0/openvdb/tree/LeafNode.h
@@ -70,14 +70,13 @@ public:
     typedef boost::shared_ptr<LeafNode>  Ptr;
     typedef util::NodeMask<Log2Dim>      NodeMaskType;
 
-    static const Index
-        LOG2DIM     = Log2Dim,      // needed by parent nodes
-        TOTAL       = Log2Dim,      // needed by parent nodes
-        DIM         = 1 << TOTAL,   // dimension along one coordinate direction
-        NUM_VALUES  = 1 << 3 * Log2Dim,
-        NUM_VOXELS  = NUM_VALUES,   // total number of voxels represented by this node
-        SIZE        = NUM_VALUES,
-        LEVEL       = 0;            // level 0 = leaf
+	static const Index LOG2DIM = Log2Dim;      // needed by parent nodes
+	static const Index TOTAL = Log2Dim;      // needed by parent nodes
+	static const Index DIM = 1 << TOTAL;   // dimension along one coordinate direction
+	static const Index NUM_VALUES = 1 << 3 * Log2Dim;
+	static const Index NUM_VOXELS = NUM_VALUES;   // total number of voxels represented by this node
+	static const Index SIZE = NUM_VALUES;
+	static const Index LEVEL = 0;            // level 0 = leaf
 
     /// @brief ValueConverter<T>::Type is the type of a LeafNode having the same
     /// dimensions as this node but a different value type, T.
