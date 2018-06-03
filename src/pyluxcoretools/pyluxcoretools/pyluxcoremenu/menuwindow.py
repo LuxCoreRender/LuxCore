9932daea9591286f58faeb577065fb2739e7aaaf
David Bucciarelli <dade916@gmail.com>
2018-06-03 10:51:01 +0200

    Version v2.1alpha1

--- src/pyluxcoretools/pyluxcoretools/pyluxcoremenu/menuwindow.py
+++ src/pyluxcoretools/pyluxcoretools/pyluxcoremenu/menuwindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/david/projects/luxcorerender/LuxCore/src/pyluxcoretools/pyluxcoretools/pyluxcoremenu/menuwindow.ui'
 #
-# Created: Mon May  7 16:37:52 2018
+# Created: Sun Jun  3 10:43:14 2018
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
