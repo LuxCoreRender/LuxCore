9fa314cec04dd1e4ce880e2be8de2ba9c2226764
David Bucciarelli <dade916@gmail.com>
2018-10-14 15:51:48 +0200

    Testing Azure pipeline build

--- scripts/azurepipeline/build.sh
+++ scripts/azurepipeline/build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-ls
-pwd
+# Install deps
+sudo apt-get -qq update
+sudo apt-get install -y git cmake g++ flex bison libbz2-dev libopenimageio-dev libtiff5-dev libpng12-dev libgtk-3-dev libopenexr-dev libgl1-mesa-dev python3-dev python3-pip python3-numpy
+# Install Pillow
+sudo pip3 install pillow
+
+# Clone LinuxCompile
+git clone https://github.com/LuxCoreRender/LinuxCompile.git
+# Clone LuxCore (this is a bit a waste but LinuxCompile procedure
+# doesn't work with symbolic links)
+git clone https://github.com/LuxCoreRender/LuxCore.git
+
+# Set up paths
+cd LinuxCompile
+ln -s .. LuxCore
+./build-64-sse2 LuxCore
