c3f3d27fe4e06ea3a56aedaaade248c8d1cc4c34
David Bucciarelli <dade916@gmail.com>
2018-10-14 18:54:42 +0200

    Testing Azure pipeline build

--- scripts/azurepipeline/build.sh
+++ scripts/azurepipeline/build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# Install deps
-sudo apt-get -qq update
-sudo apt-get install -y wget git cmake g++ flex bison libbz2-dev libopenimageio-dev libtiff5-dev libpng12-dev libgtk-3-dev libopenexr-dev libgl1-mesa-dev python3-dev python3-pip python3-numpy
+g++
+whereis g++
+ls -l /usrbin/g++
 
-wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
-tar xf boost_1_67_0.tar.gz
-cd boost_1_67_0
-./bootstrap.sh
-./b2
+
+# Install deps
+#sudo apt-get -qq update
+#sudo apt-get install -y wget git cmake g++ flex bison libbz2-dev libopenimageio-dev libtiff5-dev libpng12-dev libgtk-3-dev libopenexr-dev libgl1-mesa-dev python3-dev python3-pip python3-numpy
+
+#wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
+#tar xf boost_1_67_0.tar.gz
+#cd boost_1_67_0
+#./bootstrap.sh
+#./b2
 
 # Clone LinuxCompile
 #git clone https://github.com/LuxCoreRender/LinuxCompile.git
