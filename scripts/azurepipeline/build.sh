ec59781d2378d3872a7c5cc2b6c9e93595955d77
David Bucciarelli <dade916@gmail.com>
2018-10-14 16:27:43 +0200

    Testing Azure pipeline build

--- scripts/azurepipeline/build.sh
+++ scripts/azurepipeline/build.sh
@@ -2,9 +2,7 @@
 
 # Install deps
 sudo apt-get -qq update
-sudo apt-get install -y git cmake g++ flex bison libbz2-dev libopenimageio-dev libtiff5-dev libpng12-dev libgtk-3-dev libopenexr-dev libgl1-mesa-dev python3-dev python3-pip python3-numpy
-# Install Pillow
-sudo pip3 install pillow
+sudo apt-get install -y git cmake flex bison libbz2-dev libopenimageio-dev libtiff5-dev libpng12-dev libgtk-3-dev libopenexr-dev libgl1-mesa-dev python3-dev python3-pip python3-numpy
 
 # Clone LinuxCompile
 git clone https://github.com/LuxCoreRender/LinuxCompile.git
