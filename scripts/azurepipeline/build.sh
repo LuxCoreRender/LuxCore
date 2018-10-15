02c5b0528677df89e744d3e07ede11db2bb6b2a7
David Bucciarelli <dade916@gmail.com>
2018-10-15 11:59:17 +0200

    Testing Azure pipeline build

--- scripts/azurepipeline/build.sh
+++ scripts/azurepipeline/build.sh
@@ -4,18 +4,12 @@
 sudo apt-get -qq update
 sudo apt-get install -y wget git cmake g++ flex bison libbz2-dev libopenimageio-dev libtiff5-dev libpng12-dev libgtk-3-dev libopenexr-dev libgl1-mesa-dev python3-dev python3-pip python3-numpy
 
-wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
-tar xf boost_1_67_0.tar.gz
-cd boost_1_67_0
-./bootstrap.sh
-./b2
-
 # Clone LinuxCompile
-#git clone https://github.com/LuxCoreRender/LinuxCompile.git
+git clone https://github.com/LuxCoreRender/LinuxCompile.git
 
 # Set up paths
-#cd LinuxCompile
+cd LinuxCompile
 # Clone LuxCore (this is a bit a waste but LinuxCompile procedure
 # doesn't work with symbolic links)
-#git clone .. LuxCore
-#./build-64-sse2 LuxCore
+git clone .. LuxCore
+./build-64-sse2 LuxCore
