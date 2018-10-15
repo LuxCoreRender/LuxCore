e2c06b81d5a55bb1bfb733209a88d989f7dadd67
David Bucciarelli <dade916@gmail.com>
2018-10-15 11:38:08 +0200

    Testing Azure pipeline build

--- scripts/azurepipeline/build.sh
+++ scripts/azurepipeline/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install deps
-apt-get -qq update
-apt-get install -y wget git cmake g++ flex bison libbz2-dev libopenimageio-dev libtiff5-dev libpng12-dev libgtk-3-dev libopenexr-dev libgl1-mesa-dev python3-dev python3-pip python3-numpy
+sudo apt-get -qq update
+sudo apt-get install -y wget git cmake g++ flex bison libbz2-dev libopenimageio-dev libtiff5-dev libpng12-dev libgtk-3-dev libopenexr-dev libgl1-mesa-dev python3-dev python3-pip python3-numpy
 
 wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
 tar xf boost_1_67_0.tar.gz
