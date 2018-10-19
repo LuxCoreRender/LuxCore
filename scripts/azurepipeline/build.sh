3f44b4d170e962e268be139f0d519682e98375cf
David Bucciarelli <dade916@gmail.com>
2018-10-19 12:51:10 +0200

    Azure pipeline now compile the OpenCL version too

--- scripts/azurepipeline/build.sh
+++ scripts/azurepipeline/build.sh
@@ -2,16 +2,32 @@
 
 # Install deps
 sudo apt-get -qq update
-sudo apt-get install -y wget libtool git cmake3 g++ flex bison libbz2-dev libopenimageio-dev libtiff5-dev libpng12-dev libgtk-3-dev libopenexr-dev libgl1-mesa-dev python3-dev python3-pip python3-numpy
+sudo apt-get install -y wget libtool git cmake3 g++ flex bison libbz2-dev libopenimageio-dev libtiff5-dev libpng12-dev libgtk-3-dev libopenexr-dev libgl1-mesa-dev python3-dev python3-pip python3-numpy ocl-icd-opencl-dev
 
 # Clone LinuxCompile
 git clone https://github.com/LuxCoreRender/LinuxCompile.git
 
 # Set up paths
 cd LinuxCompile
+
+#==========================================================================
+# Compiling OpenCL-less version"
+#==========================================================================
+
 # Clone LuxCore (this is a bit a waste but LinuxCompile procedure
 # doesn't work with symbolic links)
 git clone .. LuxCore
 ./build-64-sse2 LuxCore
 mv target-64-sse2/LuxCore.tar.bz2 target-64-sse2/luxcorerender-latest-linux64.tar.bz2
+
+#==========================================================================
+# Compiling OpenCL version"
+#==========================================================================
+
+# Clone LuxCore (this is a bit a waste but LinuxCompile procedure
+# doesn't work with symbolic links)
+git clone .. LuxCore-opencl
+./build-64-sse2 LuxCore-opencl
+mv target-64-sse2/LuxCore-opencl.tar.bz2 target-64-sse2/luxcorerender-latest-linux64-opencl.tar.bz2
+
 cd ..
