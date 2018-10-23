a32beac3f9b7c8ce81399dfdb859f967aef8720e
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2018-10-23 13:36:11 +0200

    Fixed problems in Windows pipeline

--- scripts/azurepipeline/windows/build.cmd
+++ scripts/azurepipeline/windows/build.cmd
@@ -6,13 +6,12 @@ git clone --branch master https://github.com/LuxCoreRender/WindowsCompileDeps .\
 
 mklink /J Luxcore .\s
 
-pip --version
 pip install pyinstaller
 pip install numpy==1.12.1
-.\WindowsCompile\support\bin\wget.exe https://download.lfd.uci.edu/pythonlibs/h2ufg7oq/PySide-1.2.4-cp35-cp35m-win_amd64.whl -O PySide-1.2.4-cp35-cp35m-win_amd64.whl
+.\WindowsCompile\support\bin\wget.exe --user-agent="Mozilla/5.0" https://download.lfd.uci.edu/pythonlibs/h2ufg7oq/PySide-1.2.4-cp35-cp35m-win_amd64.whl -O PySide-1.2.4-cp35-cp35m-win_amd64.whl
 pip install PySide-1.2.4-cp35-cp35m-win_amd64.whl
 
 cd WindowsCompile
-call .\cmake-build-x64.cmd /cmake-only /no-ocl
+call .\cmake-build-x64.bat /cmake-only /no-ocl
 ::mv target-64-sse2/LuxCore.tar.bz2 target-64-sse2/luxcorerender-latest-linux64.tar.bz2
 cd ..
