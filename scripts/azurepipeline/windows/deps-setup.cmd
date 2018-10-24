cdf3268e601c43188b79059585e5a4be84e878e7
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2018-10-24 19:38:25 +0200

    Reworked building scripts for Azure pipeline

---
+++ scripts/azurepipeline/windows/deps-setup.cmd
@@ -0,0 +1,17 @@
+:: Setting up dependencies
+cd ..
+git lfs install
+git clone --branch master https://github.com/LuxCoreRender/WindowsCompile .\WindowsCompile
+git clone --branch master https://github.com/LuxCoreRender/WindowsCompileDeps .\WindowsCompileDeps
+
+mklink /J Luxcore .\s
+
+pip install --upgrade setuptools
+pip install --upgrade pywin32
+pip install pyinstaller
+pip install numpy==1.12.1
+.\WindowsCompile\support\bin\wget.exe --user-agent="Mozilla/5.0" https://download.lfd.uci.edu/pythonlibs/h2ufg7oq/PySide-1.2.4-cp35-cp35m-win_amd64.whl -O PySide-1.2.4-cp35-cp35m-win_amd64.whl
+pip install PySide-1.2.4-cp35-cp35m-win_amd64.whl
+
+.\WindowsCompile\support\bin\wget.exe https://github.com/GPUOpen-LibrariesAndSDKs/OCL-SDK/releases/download/1.0/OCL_SDK_Light_AMD.exe
+OCL_SDK_Light_AMD.exe /SP- /SILENT /SUPPRESSMSGBOXES
