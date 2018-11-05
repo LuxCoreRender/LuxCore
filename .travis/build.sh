ef423b30615f4dce9379fd556bd704bf66a8f703
David Bucciarelli <dade916@gmail.com>
2018-11-05 16:34:04 +0100

    Removed Travis-CI build for Linux (replaced by Azure pipeline)

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-  cmake -DBOOST_SEARCH_PATH=`pwd`/target-64-sse2 \
-  -DOPENIMAGEIO_ROOT_DIR=`pwd`/target-64-sse2 \
-  -DOPENIMAGEIO_ROOT_DIR=`pwd`/target-64-sse2 \
-  -DEMBREE_SEARCH_PATH=`pwd`/target-64-sse2 \
-  -DBLOSC_SEARCH_PATH=`pwd`/target-64-sse2 \
-  -DTBB_SEARCH_PATH=`pwd`/target-64-sse2 \
-  -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.4m.so \
-  -DPYTHON_INCLUDE_DIR=/usr/include/python3.4m .
-  make -j 2
-
-  # The unit tests take too much time
-  #make tests_subset
-elif [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   cmake -G Xcode -DLUXRAYS_DISABLE_OPENCL=1 -DOSX_DEPENDENCY_ROOT=$DEPS_SOURCE .
   cmake --build . --config Release
 fi
