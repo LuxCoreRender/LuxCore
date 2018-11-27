78a6ba12d40f5106b8a0953d842bf15992159785
Robert Brown <hi@robbrown.io>
2018-11-26 19:40:51 -0800

    Fix Permissions and missing freeimage
    Prior to this commit the azure pipeline failed because the script files
    did not have executable permissions and free image was missing for testing
    image file differences. This commit fixes that by adding executible script
    file permissions and adding freeimage to the install scrpt.

--- scripts/azurepipeline/macos/install.sh
+++ scripts/azurepipeline/macos/install.sh
@@ -4,6 +4,7 @@
 brew install bison
 brew outdated python3 || brew upgrade python3
 brew outdated cmake || brew upgrade cmake
+brew install freeimage
 brew install pyside
 brew install numpy --with-python3
 sudo pip3 install pillow
