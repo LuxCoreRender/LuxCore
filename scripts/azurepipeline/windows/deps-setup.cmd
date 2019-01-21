932092760cf21ce1d7322d29867b0a11956b9017
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2019-01-21 20:44:14 +0100

    Update PySide2 dependency in Windows build pipeline

--- scripts/azurepipeline/windows/deps-setup.cmd
+++ scripts/azurepipeline/windows/deps-setup.cmd
@@ -10,8 +10,7 @@ pip install --upgrade setuptools
 pip install --upgrade pywin32
 pip install pyinstaller
 pip install numpy==1.12.1
-.\WindowsCompile\support\bin\wget.exe --user-agent="Mozilla/5.0" https://download.lfd.uci.edu/pythonlibs/h2ufg7oq/PySide-1.2.4-cp35-cp35m-win_amd64.whl -O PySide-1.2.4-cp35-cp35m-win_amd64.whl
-pip install PySide-1.2.4-cp35-cp35m-win_amd64.whl
+pip install PySide2
 
 .\WindowsCompile\support\bin\wget.exe https://github.com/GPUOpen-LibrariesAndSDKs/OCL-SDK/files/1406216/lightOCLSDK.zip
 .\WindowsCompile\support\bin\7za.exe x -oWindowsCompile\OCL_SDK_Light lightOCLSDK.zip
