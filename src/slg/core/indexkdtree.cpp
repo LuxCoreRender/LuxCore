6c3dc6ec854e5c4e28417ab94dc45058150aa8e3
David Bucciarelli <dade916@gmail.com>
2019-01-30 17:54:04 +0100

    Fixed compilation

--- src/slg/core/indexkdtree.cpp
+++ src/slg/core/indexkdtree.cpp
@@ -21,9 +21,6 @@
 #include "luxrays/core/geometry/bbox.h"
 #include "slg/core/indexkdtree.h"
 
-// Required for explicit instantiations
-#include "slg/engines/caches/photongi/photongicache.h"
-
 using namespace std;
 using namespace luxrays;
 using namespace slg;
@@ -120,95 +117,6 @@ IndexKdTree<T>::Build(const u_int nodeIndex, const u_int start, const u_int end,
 			KdTreeNodeData_SetRightChild(arrayNodes[nodeIndex].nodeData, KdTreeNodeData_NULL_INDEX);
 			
 			assert (KdTreeNodeData_GetRightChild(arrayNodes[nodeIndex].nodeData) == KdTreeNodeData_NULL_INDEX);
-		}
-	}
-}
-// Iterative implementation
-void PGICPhotonKdTree::GetAllNearEntries(vector<NearPhoton> &entries,
-		const Point &p, const Normal &n, float &maxDistance2) const {
-	const int stackSize = 64;
-
-	u_int nodeIndexStack[stackSize];
-
-	int stackCurrentIndex = 0;
-	nodeIndexStack[stackCurrentIndex] = 0;
-
-	while (stackCurrentIndex >= 0) {
-		// Pop the current node form the stack
-		const u_int currentNodeIndex = nodeIndexStack[stackCurrentIndex--];
-
-		const IndexKdTreeArrayNode &node = arrayNodes[currentNodeIndex];
-		const Photon &entry = allEntries[node.index];
-
-		const u_int axis = KdTreeNodeData_GetAxis(node.nodeData);
-
-		// Add check of the children if it is not a leaf
-		if (axis != 3) {
-			const float distance2 = Sqr(p[axis] - node.splitPos);
-
-			if (p[axis] <= node.splitPos) {
-				if (KdTreeNodeData_HasLeftChild(node.nodeData)) {
-					nodeIndexStack[++stackCurrentIndex] = currentNodeIndex + 1;
-					
-					assert (stackCurrentIndex < stackSize);
-					assert (nodeIndexStack[stackCurrentIndex] < allEntries.size());
-				}
-
-				const u_int rightChildIndex = KdTreeNodeData_GetRightChild(node.nodeData);
-				if ((distance2 < maxDistance2) && (rightChildIndex != KdTreeNodeData_NULL_INDEX)) {
-					nodeIndexStack[++stackCurrentIndex] = rightChildIndex;
-					
-					assert (stackCurrentIndex < stackSize);
-					assert (nodeIndexStack[stackCurrentIndex] < allEntries.size());
-				}
-			} else {
-				const u_int rightChildIndex = KdTreeNodeData_GetRightChild(node.nodeData);
-				if (rightChildIndex != KdTreeNodeData_NULL_INDEX) {
-					nodeIndexStack[++stackCurrentIndex] = rightChildIndex;
-					
-					assert (stackCurrentIndex < stackSize);
-					assert (nodeIndexStack[stackCurrentIndex] < allEntries.size());
-				}
-
-				if ((distance2 < maxDistance2) && KdTreeNodeData_HasLeftChild(node.nodeData)) {
-					nodeIndexStack[++stackCurrentIndex] = currentNodeIndex + 1;
-					
-					assert (stackCurrentIndex < stackSize);
-					assert (nodeIndexStack[stackCurrentIndex] < allEntries.size());
-				}
-			}
-		}
-
-		// Check the current node
-		const float distance2 = DistanceSquared(entry.p, p);
-		if ((distance2 < maxDistance2) &&
-					(Dot(n, -entry.d) > DEFAULT_COS_EPSILON_STATIC) &&
-					(Dot(n, entry.landingSurfaceNormal) > entryNormalCosAngle)) {
-			// I have found a valid entry
-
-			NearPhoton nearPhoton(&entry, distance2);
-
-			if (entries.size() < entryMaxLookUpCount) {
-				// Just add the entry
-				entries.push_back(nearPhoton);
-
-				// Check if the array is now full and sort the entries for
-				// the next addition
-				if (entries.size() == entryMaxLookUpCount)
-					make_heap(&entries[0], &entries[entryMaxLookUpCount]);
-			} else {
-				// Check if the new entry is nearer than the farthest array entry
-				if (distance2 < entries[0].distance2) {
-					// Remove the farthest array entry
-					pop_heap(&entries[0], &entries[entryMaxLookUpCount]);
-					// Add the new entry
-					entries[entryMaxLookUpCount - 1] = nearPhoton;
-					push_heap(&entries[0], &entries[entryMaxLookUpCount]);
-
-					// Update max. squared distance
-					maxDistance2 = entries[0].distance2;
-				}
-			}
 		}
 	}
 }
@@ -379,13 +287,3 @@ void PGICPhotonKdTree::GetAllNearEntriesImpl(const u_int currentNodeIndex, vecto
 	}
 }
 */
-
-//------------------------------------------------------------------------------
-// Explicit instantiations
-//------------------------------------------------------------------------------
-
-// C++ can be quite horrible...
-
-namespace slg {
-template class IndexKdTree<Photon>;
-}
