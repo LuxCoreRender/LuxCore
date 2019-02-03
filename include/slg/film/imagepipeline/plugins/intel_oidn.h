f8276f960d5295e4a8836f2c93d008d1f4ee34d9
Daniel Moreira Guimarães <danielmguimaraes@poli.ufrj.br>
2019-02-02 23:16:25 -0200

    Add initial support for the denoise library as an IMAGEPIPELINE plugin

--- include/slg/film/imagepipeline/plugins/intel_oidn.h
+++ include/slg/film/imagepipeline/plugins/intel_oidn.h
@@ -1,10 +1,14 @@
 #ifndef _SLG_INTEL_OIDN_H
 #define	_SLG_INTEL_OIDN_H
 
+#include <vector>
+
 #include <boost/serialization/export.hpp>
 
 #include <OpenImageDenoise/oidn.hpp>
 
+#include "luxrays/luxrays.h"
+#include "luxrays/core/color/color.h"
 #include "slg/film/film.h"
 #include "slg/film/imagepipeline/imagepipeline.h"
 
@@ -16,6 +20,16 @@ namespace slg {
             virtual ImagePipelinePlugin *Copy() const;
 
             virtual void Apply(Film &film, const u_int index);
+
+            friend class boost::serialization::access;
+        
+        private:
+
+            template<class Archive> void serialize(Archive &ar, const u_int version) {
+                ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(ImagePipelinePlugin);
+                // TODO: implement serialization for ImageMap
+                throw std::runtime_error("IntelOIDN serialization not yet supported");
+            }
     };
 }
 
--- src/slg/film/imagepipeline/plugins/intel_oidn.cpp
+++ src/slg/film/imagepipeline/plugins/intel_oidn.cpp
@@ -1,13 +1,51 @@
 #include "slg/film/imagepipeline/plugins/intel_oidn.h"
 
-namespace slg {
+using namespace std;
+using namespace luxrays;
+using namespace slg;
 
 ImagePipelinePlugin *IntelOIDN::Copy() const {
     return new IntelOIDN();
 }
 
 void IntelOIDN::Apply(Film &film, const u_int index) {
+    Spectrum *pixels = (Spectrum *)film.channel_IMAGEPIPELINEs[index]->GetPixels();
+
+    const size_t width = film.GetWidth();
+    const size_t height = film.GetHeight();
+
+    const u_int pixelCount = width * height;
+
+    vector<float> color(3*pixelCount); // A float for each color channel
+    vector<float> output(3*pixelCount); 
+
+    for (u_int i = 0; i < pixelCount; i += 3) {
+                color[i] = pixels[i].c[0];
+				color[i + 1] = pixels[i].c[1];
+				color[i + 2] = pixels[i].c[2];
+	}
+
     oidn::DeviceRef device = oidn::newDevice();
-}
+
+    const char* errorMessage;
+    if (device.getError(errorMessage) != oidn::Error::None)
+      throw std::runtime_error(errorMessage);
+    device.set("numThreads", -1); //Automatic for now
+    device.commit();
+
+    // TODO: HDR support
+    oidn::FilterRef filter = device.newFilter("RT");
+    filter.setImage("color", color.data(), oidn::Format::Float3, width, height);
+    filter.setImage("output", output.data(), oidn::Format::Float3, width, height);
+    filter.commit();
+
+    filter.execute();
+
+    for (u_int i = 0; i < pixelCount; i += 3) {
+                pixels[i].c[0] = color[i];
+				pixels[i].c[1] = color[i + 1];
+                pixels[i].c[2] = color[i + 2];
+	}
+
 
 }
