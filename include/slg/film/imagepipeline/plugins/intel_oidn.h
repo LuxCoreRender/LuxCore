2f768ff618202bdc2892b579c2b0c088c75d1a26
David Bucciarelli <dade916@gmail.com>
2019-02-04 11:25:19 +0100

    Added the support for ALBEDO and AVG_SHADING_NORMAL AOV to Intel Oidn plugin

--- include/slg/film/imagepipeline/plugins/intel_oidn.h
+++ include/slg/film/imagepipeline/plugins/intel_oidn.h
@@ -1,3 +1,21 @@
+/***************************************************************************
+ * Copyright 1998-2018 by authors (see AUTHORS.txt)                        *
+ *                                                                         *
+ *   This file is part of LuxCoreRender.                                   *
+ *                                                                         *
+ * Licensed under the Apache License, Version 2.0 (the "License");         *
+ * you may not use this file except in compliance with the License.        *
+ * You may obtain a copy of the License at                                 *
+ *                                                                         *
+ *     http://www.apache.org/licenses/LICENSE-2.0                          *
+ *                                                                         *
+ * Unless required by applicable law or agreed to in writing, software     *
+ * distributed under the License is distributed on an "AS IS" BASIS,       *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ * See the License for the specific language governing permissions and     *
+ * limitations under the License.                                          *
+ ***************************************************************************/
+
 #ifndef _SLG_INTEL_OIDN_H
 #define	_SLG_INTEL_OIDN_H
 
@@ -14,35 +32,35 @@
 
 namespace slg {
 
-    template<typename T>
-    using shared_vector = std::shared_ptr<std::vector<T>>;
+//------------------------------------------------------------------------------
+//Intel Open Image Denoise
+//------------------------------------------------------------------------------
 
-    class IntelOIDN : public ImagePipelinePlugin {
-        public:
+class IntelOIDN : public ImagePipelinePlugin {
+	public:
+		IntelOIDN();
 
-            IntelOIDN();
+		virtual ImagePipelinePlugin *Copy() const;
 
-            virtual ImagePipelinePlugin *Copy() const;
+		virtual void Apply(Film &film, const u_int index);
 
-            virtual void Apply(Film &film, const u_int index);
+		friend class boost::serialization::access;
 
-            friend class boost::serialization::access;
-        
-        private:
+	private:
 
-            oidn::DeviceRef device;
-            oidn::FilterRef filter;
+		oidn::DeviceRef device;
+		oidn::FilterRef filter;
 
-            template<class Archive> void serialize(Archive &ar, const u_int version) {
-                ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(ImagePipelinePlugin);
-                // TODO: implement serialization for ImageMap
-                throw std::runtime_error("IntelOIDN serialization not yet supported");
-            }
-    };
+		template<class Archive> void serialize(Archive &ar, const u_int version) {
+			ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(ImagePipelinePlugin);
+		}
+};
+
 }
 
 
 BOOST_CLASS_VERSION(slg::IntelOIDN, 1)
+
 BOOST_CLASS_EXPORT_KEY(slg::IntelOIDN)
 
 #endif /* _SLG_INTEL_OIDN_H */
--- src/slg/film/imagepipeline/plugins/intel_oidn.cpp
+++ src/slg/film/imagepipeline/plugins/intel_oidn.cpp
@@ -1,12 +1,34 @@
+/***************************************************************************
+ * Copyright 1998-2018 by authors (see AUTHORS.txt)                        *
+ *                                                                         *
+ *   This file is part of LuxCoreRender.                                   *
+ *                                                                         *
+ * Licensed under the Apache License, Version 2.0 (the "License");         *
+ * you may not use this file except in compliance with the License.        *
+ * You may obtain a copy of the License at                                 *
+ *                                                                         *
+ *     http://www.apache.org/licenses/LICENSE-2.0                          *
+ *                                                                         *
+ * Unless required by applicable law or agreed to in writing, software     *
+ * distributed under the License is distributed on an "AS IS" BASIS,       *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ * See the License for the specific language governing permissions and     *
+ * limitations under the License.                                          *
+ ***************************************************************************/
+
+#include <boost/format.hpp>
+
 #include "slg/film/imagepipeline/plugins/intel_oidn.h"
 
 using namespace std;
 using namespace luxrays;
 using namespace slg;
 
-ImagePipelinePlugin *IntelOIDN::Copy() const {
-    return new IntelOIDN();
-}
+//------------------------------------------------------------------------------
+//Intel Open Image Denoise
+//------------------------------------------------------------------------------
+
+BOOST_CLASS_EXPORT_IMPLEMENT(slg::IntelOIDN)
 
 IntelOIDN::IntelOIDN() {
     device = oidn::newDevice();
@@ -14,68 +36,58 @@ IntelOIDN::IntelOIDN() {
     filter = device.newFilter("RT");
 }
 
+ImagePipelinePlugin *IntelOIDN::Copy() const {
+    return new IntelOIDN();
+}
+
 void IntelOIDN::Apply(Film &film, const u_int index) {
     Spectrum *pixels = (Spectrum *)film.channel_IMAGEPIPELINEs[index]->GetPixels();
 
-    size_t width = film.GetWidth();
-    size_t height = film.GetHeight();
+    const u_int width = film.GetWidth();
+    const u_int height = film.GetHeight();
+    const u_int pixelCount = width * height;
 
-    u_int pixelCount = width * height;
-    shared_vector<char> colorBuffer;
-    shared_vector<char> outputBuffer;
-    shared_vector<char> albedoBuffer;
-    shared_vector<char> normalBuffer;
-
-    colorBuffer = std::make_shared<std::vector<char>>(3*pixelCount * sizeof(float));
-    outputBuffer = std::make_shared<std::vector<char>>(3*pixelCount * sizeof(float));
-    albedoBuffer = std::make_shared<std::vector<char>>(3*pixelCount * sizeof(float));
-    normalBuffer = std::make_shared<std::vector<char>>(3*pixelCount * sizeof(float));
-
-    float* color;
-    float* output;
-    float* albedo;
-    float* normal;
-
-    color = (float*)colorBuffer->data();
-    output = (float*)outputBuffer->data();
-    albedo = (float*)albedoBuffer->data();
-    normal = (float*)normalBuffer->data();
-
-    SLG_LOG("[IntelOIDNPlugin] Copying Film to input buffer");
-    for (u_int i = 0; i < pixelCount; ++i) {
-        const u_int i3 = i * 3;
-        color[i3] = pixels[i].c[0];
-        color[i3 + 1] = pixels[i].c[1];
-        color[i3 + 2] = pixels[i].c[2];
-    }
+    vector<float> outputBuffer(3 * pixelCount);
+    vector<float> albedoBuffer;
+    vector<float> normalBuffer;
 
     filter.set("hdr", true);
-    filter.setImage("color", color, oidn::Format::Float3, width, height);
+    filter.setImage("color", (float *)pixels, oidn::Format::Float3, width, height);
     if (film.HasChannel(Film::ALBEDO)) {
-        filter.setImage("albedo", albedo, oidn::Format::Float3, width, height);
-    } else {SLG_LOG("[IntelOIDNPlugin] Warning: ALBEDO AOV not found")}
+		albedoBuffer.resize(3 * pixelCount);
+		for (u_int i = 0; i < pixelCount; ++i)
+			film.channel_ALBEDO->GetWeightedPixel(i, &albedoBuffer[i * 3]);
+		
+        filter.setImage("albedo", &albedoBuffer[0], oidn::Format::Float3, width, height);
+    } else
+		SLG_LOG("[IntelOIDNPlugin] Warning: ALBEDO AOV not found");
 
     if (film.HasChannel(Film::AVG_SHADING_NORMAL)) {
-        filter.setImage("normal", normal, oidn::Format::Float3, width, height);
-    } else {SLG_LOG("[IntelOIDNPlugin] Warning: AVG_SHADING_NORMAL AOV not found")}
+		normalBuffer.resize(3 * pixelCount);
+		for (u_int i = 0; i < pixelCount; ++i)
+			film.channel_AVG_SHADING_NORMAL->GetWeightedPixel(i, &albedoBuffer[i * 3]);
+
+        filter.setImage("normal", &normalBuffer[0], oidn::Format::Float3, width, height);
+    } else
+		SLG_LOG("[IntelOIDNPlugin] Warning: AVG_SHADING_NORMAL AOV not found");
     
-    filter.setImage("output", output, oidn::Format::Float3, width, height);
+    filter.setImage("output", &outputBuffer[0], oidn::Format::Float3, width, height);
     filter.commit();
     
-    SLG_LOG("[IntelOIDNPlugin] Executing filter");
+    SLG_LOG("IntelOIDNPlugin executing filter");
+	const double startTime = WallClockTime();
     filter.execute();
+	SLG_LOG("IntelOIDNPlugin apply took: " << (boost::format("%.1f") % (WallClockTime() - startTime)) << "secs");
 
-    const char* errorMessage;
+    const char *errorMessage;
     if (device.getError(errorMessage) != oidn::Error::None)
-         SLG_LOG("[IntelOIDNPlugin] Error: " << errorMessage);
+         SLG_LOG("IntelOIDNPlugin error: " << errorMessage);
 
-    SLG_LOG("[IntelOIDNPlugin] Copying Film to output buffer");
+    SLG_LOG("IntelOIDNPlugin copying output buffer");
     for (u_int i = 0; i < pixelCount; ++i) {
         const u_int i3 = i * 3;
-        pixels[i].c[0] = output[i3];
-        pixels[i].c[1] = output[i3 + 1];
-        pixels[i].c[2] = output[i3 + 2];
+        pixels[i].c[0] = outputBuffer[i3];
+        pixels[i].c[1] = outputBuffer[i3 + 1];
+        pixels[i].c[2] = outputBuffer[i3 + 2];
 	}
-
-
 }
