07633ed1c1a3b187c3023860dc1c41f5e15ccbe3
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2019-02-11 11:35:10 +0100

    Windows builds now use previous PySide2 version to avoid problem with PyInstaller-generated executable

--- scripts/azurepipeline/windows/deps-setup.cmd
+++ scripts/azurepipeline/windows/deps-setup.cmd
@@ -10,7 +10,7 @@ pip install --upgrade setuptools
 pip install --upgrade pywin32
 pip install pyinstaller
 pip install numpy==1.12.1
-pip install PySide2
+pip install PySide2==5.12.0
 
 .\WindowsCompile\support\bin\wget.exe https://github.com/GPUOpen-LibrariesAndSDKs/OCL-SDK/files/1406216/lightOCLSDK.zip
 .\WindowsCompile\support\bin\7za.exe x -oWindowsCompile\OCL_SDK_Light lightOCLSDK.zip
