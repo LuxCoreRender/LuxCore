2f6319732e31e25515a796c1ba16a6eac802fcc3
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2019-03-09 19:07:04 +0100

    Intriducing support for automatic official releases

--- scripts/azurepipeline/windows/post-build.cmd
+++ scripts/azurepipeline/windows/post-build.cmd
@@ -1,12 +1,19 @@
 :: Gathering and packing binaries
+if "RELEASE_BUILD" EQU "TRUE" (
+    for /f "tokens=2 delims=_" %%a in ('git tag --points-at HEAD') do set GITHUB_TAG=%%a
+) else (
+    set GITHUB_TAG=latest
+)
+echo %GITHUB_TAG%
 cd ..\WindowsCompile
 call create-standalone.bat
 
 if "%1" EQU "/no-ocl" (
-    set LUX_LATEST=luxcorerender-latest-win64
+    set LUX_LATEST=luxcorerender-%GITHUB_TAG%-win64
 ) else (
-    set LUX_LATEST=luxcorerender-latest-win64-opencl
+    set LUX_LATEST=luxcorerender-%GITHUB_TAG%-win64-opencl
 )
 
 move %DIR% %LUX_LATEST%
 .\support\bin\7za.exe a %LUX_LATEST%.zip %LUX_LATEST%
+copy %LUX_LATEST%.zip %BUILD_ARTIFACTSTAGINGDIRECTORY%
