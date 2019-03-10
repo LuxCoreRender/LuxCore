b126ce9c60277f002647e643078e7562b767fefe
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2019-03-10 18:32:49 +0100

    More debugging

--- scripts/azurepipeline/windows/post-build.cmd
+++ scripts/azurepipeline/windows/post-build.cmd
@@ -2,6 +2,7 @@
 if "%RELEASE_BUILD%" EQU "TRUE" (
     echo This is a release build
     echo Git commit is %BUILD_SOURCEVERSION%
+    git tag --points-at %BUILD_SOURCEVERSION%
     for /f "tokens=2 delims=_" %%a in ('git tag --points-at %BUILD_SOURCEVERSION%') do set GITHUB_TAG=%%a
     if "%GITHUB_TAG%" EQU "" (
         echo A release build must be associated with a Github tag
