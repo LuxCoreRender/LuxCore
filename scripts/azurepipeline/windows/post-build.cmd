8ac4318241dec5964b3165ff289a879e3cacb119
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2019-03-11 16:31:52 +0100

    Yet another test

--- scripts/azurepipeline/windows/post-build.cmd
+++ scripts/azurepipeline/windows/post-build.cmd
@@ -1,15 +1,10 @@
 :: Gathering and packing binaries
 echo %RELEASE_BUILD%
 if "%RELEASE_BUILD%" EQU "TRUE" (
-    echo This is an official release build
-    echo Git commit is %BUILD_SOURCEVERSION%
+    echo This is a release build
+    echo %BUILD_SOURCEVERSION%
     git tag --points-at %BUILD_SOURCEVERSION%
     for /f "tokens=2 delims=_" %%a in ('git tag --points-at %BUILD_SOURCEVERSION%') do set GITHUB_TAG=%%a
-    echo Github tag is %GITHUB_TAG%
-    if "%GITHUB_TAG%" EQU "" (
-        echo An official release build must be associated with a Github tag
-        exit /b 1
-    )
 ) else (
     set GITHUB_TAG=latest
 )
