afc392dc819fb1207a733b5f160218bd0784b38c
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2019-03-30 19:25:45 +0100

    Introduced more complete and robust checks on version tags

--- scripts/azurepipeline/detect-build-type.sh
+++ scripts/azurepipeline/detect-build-type.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-# Detect release type (official or daily) and set version tag
-echo "$RELEASE_BUILD"
+# Called by "Release_Pipeline", the build pipeline for official releases
+# Detect official release type (alpha, beta or final) and set version string
+# One, and only one, tag in the form "luxcorerender_v*" is needed, 
+# otherwise the official release build aborts.
+
 if [[ $RELEASE_BUILD == "TRUE" ]] ; then
     echo "This is an official release build"
-    echo $BUILD_SOURCEVERSION
-    VERSION_STRING=$(git tag --points-at $BUILD_SOURCEVERSION | cut -d'_' -f 2)
-else
-    VERSION_STRING="latest"
+    echo "Commit ID: $BUILD_SOURCEVERSION"
+    TAGS=$(git tag --points-at $BUILD_SOURCEVERSION)
+    for tag in $TAGS
+    do
+        if [[ $tag != "latest" ]] && [[ $tag == "luxcorerender_v"* ]] ; then
+            if [[ -z "$VERSION_STRING" ]] ; then
+                VERSION_STRING=$(echo $tag | cut -d'_' -f 2)
+            else
+                echo "Multiple git tags identify different versions, aborting..."
+                exit 1
+            fi
+        fi
+    done
 fi
 
 echo "$VERSION_STRING"
 if [[ -z "$VERSION_STRING" ]] ; then
-    echo "No git tag found, one is needed for an official release"
+    echo "No suitable git tag found"
+    echo "For an official release, a tag in the form 'luxcorerender_v*' is needed"
     exit 1
 fi
 
@@ -24,5 +37,6 @@ else
     FINAL="TRUE"
 fi
 
+# make FINAL and VERSION_STRING variables available for other pipeline jobs
 echo "##vso[task.setvariable variable=final;isOutput=true]$FINAL"
 echo "##vso[task.setvariable variable=version_string;isOutput=true]$VERSION_STRING"
