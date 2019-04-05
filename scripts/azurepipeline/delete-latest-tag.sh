b4ba9cb10d76646b66d05d37759f7794c8715a5a
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2019-04-05 07:50:20 +0200

    Only activity on master branch can now modify latest release

--- scripts/azurepipeline/delete-latest-tag.sh
+++ scripts/azurepipeline/delete-latest-tag.sh
@@ -3,8 +3,6 @@
 # Called by "LuxCoreRender.LuxCore", the build pipeline for daily builds
 # Deletes the "latest" tag (if existing) from LuxCore git repo, so that it is
 # then recreated at the current commit by the following release pipeline.
-# Runs only if at least one of the build jobs was successful 
-# and pipeline has not been triggered by a pull request.
 
 TAG=$(git tag -l latest)
 if [[ $TAG == "latest" ]] ; then
