1ccf565c98a545ffabff616a42d03784d9bfd063
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2019-04-10 11:50:36 +0200

    Fixed Linux SDK build

--- scripts/azurepipeline/linux/build.sh
+++ scripts/azurepipeline/linux/build.sh
@@ -25,7 +25,7 @@ cd LinuxCompile
 # Clone LuxCore (this is a bit a waste but LinuxCompile procedure
 # doesn't work with symbolic links)
 git clone .. LuxCore$SDK_BUILD
-./build-64-sse2 LuxCore
+./build-64-sse2 LuxCore$SDK_BUILD
 cp target-64-sse2/LuxCore.tar.bz2 target-64-sse2/luxcorerender-$VERSION_STRING-linux64$SDK_BUILD.tar.bz2
 mv target-64-sse2/LuxCore.tar.bz2 $BUILD_ARTIFACTSTAGINGDIRECTORY/luxcorerender-$VERSION_STRING-linux64$SDK_BUILD.tar.bz2
 
@@ -36,7 +36,7 @@ mv target-64-sse2/LuxCore.tar.bz2 $BUILD_ARTIFACTSTAGINGDIRECTORY/luxcorerender-
 # Clone LuxCore (this is a bit a waste but LinuxCompile procedure
 # doesn't work with symbolic links)
 git clone .. LuxCore-opencl$SDK_BUILD
-./build-64-sse2 LuxCore-opencl 5
+./build-64-sse2 LuxCore-opencl$SDK_BUILD 5
 cp target-64-sse2/LuxCore-opencl.tar.bz2 target-64-sse2/luxcorerender-$VERSION_STRING-linux64-opencl$SDK_BUILD.tar.bz2
 mv target-64-sse2/LuxCore-opencl.tar.bz2 $BUILD_ARTIFACTSTAGINGDIRECTORY/luxcorerender-$VERSION_STRING-linux64-opencl$SDK_BUILD.tar.bz2
 
