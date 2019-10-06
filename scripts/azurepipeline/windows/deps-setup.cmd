b1982822ae42d787e5a61d0c2bc05d65fb3c4815
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2019-10-06 14:43:39 +0200

    Removed support for Blender 2.79b binaries, default is now Blender 2.8x

--- scripts/azurepipeline/windows/deps-setup.cmd
+++ scripts/azurepipeline/windows/deps-setup.cmd
@@ -1,12 +1,5 @@
 :: Setting up dependencies
 
-if "%BLENDER280%" EQU "TRUE" (
-    git checkout blender2.80
-    git config user.email "email"
-    git config user.name "name"
-    git merge --no-commit origin/%BUILD_SOURCEBRANCHNAME%
-)
-
 cd ..
 git lfs install
 git clone --branch master https://github.com/LuxCoreRender/WindowsCompile .\WindowsCompile
@@ -22,16 +15,7 @@ pip install wheel
 pip install pyinstaller
 
 :: pyluxcoretool will use same numpy version used to build LuxCore
-for /F "tokens=2" %%a in ('python --version') do set PPP=%%a
-for /F "tokens=1,2 delims=." %%a in ("%PPP%") do (
-    set PPP1=%%a
-    set PPP2=%%b
-)
-if "%PPP1%.%PPP2%" EQU "3.5" (
-    pip install numpy==1.12.1
-) else (
-    pip install numpy==1.15.4
-)
+pip install numpy==1.15.4
 
 pip install PySide2
 
