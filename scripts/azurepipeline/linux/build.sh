a14d6a26c0e009f5dbb8858434118bb9d5f49c6c
David Bucciarelli <dade916@gmail.com>
2019-10-22 00:25:35 +0200

    Still trying to fix Azure Linux compilation #15

--- scripts/azurepipeline/linux/build.sh
+++ scripts/azurepipeline/linux/build.sh
@@ -2,8 +2,7 @@
 
 # Install deps
 sudo apt-get -qq update
-sudo apt-get install -y wget libtool git cmake g++ flex bison libbz2-dev libgtk-3-dev libopenexr-dev libgl1-mesa-dev python3-dev python3-pip python3-numpy ocl-icd-opencl-dev
-#sudo apt-get install -y wget libtool git cmake g++ flex bison libbz2-dev libopenimageio-dev libtiff5-dev libpng12-dev libgtk-3-dev libopenexr-dev libgl1-mesa-dev python3-dev python3-pip python3-numpy ocl-icd-opencl-dev
+sudo apt-get install -y wget libtool git cmake g++ flex bison libbz2-dev libgtk-3-dev libgl1-mesa-dev python3-dev python3-pip python3-numpy ocl-icd-opencl-dev
 
 # Clone LinuxCompile
 git clone https://github.com/LuxCoreRender/LinuxCompile.git
