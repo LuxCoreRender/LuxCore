9ac87cbdb8ad30217f2a69fc1111a38e81b7672e
David Bucciarelli <dade916@gmail.com>
2019-12-14 15:56:20 +0100

    First glTF exported meshes

--- samples/luxcoreui/uimenu.cpp
+++ samples/luxcoreui/uimenu.cpp
@@ -49,6 +49,8 @@ void LuxCoreApp::MenuRendering() {
 		}
 	}
 
+	ImGui::Separator();
+
 	if (session && ImGui::MenuItem("Export")) {
 		nfdchar_t *outPath = NULL;
 		nfdresult_t result = NFD_SaveDialog(NULL, NULL, &outPath);
@@ -86,16 +88,17 @@ void LuxCoreApp::MenuRendering() {
 	}
 
 	if (session && ImGui::MenuItem("Export (glTF)")) {
-//		nfdchar_t *fileName = NULL;
-//		nfdresult_t result = NFD_SaveDialog("gltf", NULL, &fileName);
-//
-//		if (result == NFD_OKAY) {
-//			LA_LOG("Export current scene to file in glTF format: " << fileName);
-//			config->ExportGLTF(fileName);
-//		}
-		config->ExportGLTF("test.gltf");
+		nfdchar_t *fileName = NULL;
+		nfdresult_t result = NFD_SaveDialog("gltf", NULL, &fileName);
+
+		if (result == NFD_OKAY) {
+			LA_LOG("Export current scene to file in glTF format: " << fileName);
+			config->ExportGLTF(fileName);
+		}
 	}
 	
+	ImGui::Separator();
+
 	if (session && ImGui::MenuItem("Bake all objects"))
 		BakeAllSceneObjects();
 
