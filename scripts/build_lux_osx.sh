c6ec21a4518559be420b426cafe0aa443e2d6288
u3dreal <48415446+u3dreal@users.noreply.github.com>
2019-12-17 03:35:14 +0100

    update test.sh for azure and hack just copied the working denoise and lib

--- scripts/build_lux_osx.sh
+++ scripts/build_lux_osx.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 #automated script build for macos 10.13 should compile and patch with dependencies in root/macos
+# set python and bison env *
+eval "$(pyenv init -)"
+pyenv shell 3.7.4
+export PATH="/usr/local/opt/bison/bin:/usr/local/bin:$PATH"
 
+# build luxcore
 rm -rf build
 
 mkdir build
@@ -14,6 +19,7 @@ make -j8
 
 cd ..
 
+# bundle things 
 rm -rf release_OSX 
 
 mkdir release_OSX
@@ -48,12 +54,16 @@ cd pyluxcore
 dylibbundler -of -b -x pyluxcore.so -d ./ -p @loader_path/
 
 ###denoise
-cp ../../macos/bin/denoise .
+cp ../../macos/mac_bundle/denoise .
 
-dylibbundler -of -b -x denoise -d ./ -p @executable_path/
+cp ../../macos/mac_bundle/libOpenImageDenoise.1.1.0.dylib .
 
-chmod 755 ./denoise
+#dylibbundler -of -b -x denoise -d ./ -p @executable_path/
+
+chmod 755 denoise
 
 cd ../../
 
-tar -czf LuxCoreRender.tar.gz ./release_OSX
+#tar -czf LuxCoreRender.tar.gz ./release_OSX
+
+hdiutil create LuxCoreRender2.3alpha0.dmg -volname "LuxCoreRender2.3alpha0" -fs HFS+ -srcfolder release_OSX/
