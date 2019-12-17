189ea91a8475cc95d7a71a4b5c8be9c91e9b1310
u3dreal <48415446+u3dreal@users.noreply.github.com>
2019-12-17 16:06:39 +0100

    Update test.sh

--- scripts/azurepipeline/macos/test.sh
+++ scripts/azurepipeline/macos/test.sh
@@ -13,7 +13,7 @@ popd
 pushd pyunittests
 
 # Add symbolic link to release pyluxcore.so
-ln -s ../build/lib/pyluxcore.so pyluxcore.so
+ln -s ../build/lib/pyluxcore.so ./pyluxcore.so
 
 # Run Tests
 python3 unittests.py
