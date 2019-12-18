36ea41d692cb9c795c2c5e849a328edc342a767f
u3dreal <48415446+u3dreal@users.noreply.github.com>
2019-12-19 00:59:18 +0100

    azure did not find path

--- scripts/azurepipeline/macos/test.sh
+++ scripts/azurepipeline/macos/test.sh
@@ -4,6 +4,7 @@
 export PATH="/usr/local/bin:$PATH"
 eval "$(pyenv init -)"
 pyenv shell 3.7.4
+BUILDDIR=`pwd`/build
 
 # Build Test Diff Tool
 pushd deps/perceptualdiff-master
@@ -14,7 +15,7 @@ popd
 pushd pyunittests
 
 # Add symbolic link to release pyluxcore.so
-ln -s ../build/lib/pyluxcore.so ./pyluxcore.so
+ln -s $BUILDDIR/lib/pyluxcore.so ./pyluxcore.so
 
 # Run Tests
 python3 unittests.py
