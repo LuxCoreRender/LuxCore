3d2b26996d28b87aec8e46c84b332428b5f4d870
u3dreal <48415446+u3dreal@users.noreply.github.com>
2019-12-22 23:09:31 +0100

    Update build.sh

--- scripts/azurepipeline/macos/build.sh
+++ scripts/azurepipeline/macos/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Fetch Artifacts
-#wget https://github.com/LuxCoreRender/MacOSCompileDeps/releases/download/luxcorerender_v2.3alpha1/MacDistFiles.tar.gz
-#tar xzf MacDistFiles.tar.gz
+wget https://github.com/LuxCoreRender/MacOSCompileDeps/releases/download/luxcorerender_v2.3alpha1/MacDistFiles.tar.gz
+tar xzf MacDistFiles.tar.gz
 
 # Set Environment Variables
 export PATH="/usr/local/opt/bison/bin:/usr/local/bin:$PATH"
@@ -16,7 +16,7 @@ eval "$(pyenv init -)"
 mkdir build
 pushd  build
 cmake -DOSX_DEPENDENCY_ROOT=$DEPS_SOURCE -DCMAKE_BUILD_TYPE=Release ..
-make -j8
+make 
 popd
 
 mkdir build_ocl
@@ -30,5 +30,5 @@ cp ./build/lib/Release/pyluxcore.so ./build_ocl
 
 pushd  build
 cmake -DLUXRAYS_DISABLE_OPENCL=1 -DOSX_DEPENDENCY_ROOT=$DEPS_SOURCE -DCMAKE_BUILD_TYPE=Release ..
-make -j8
+make 
 popd
