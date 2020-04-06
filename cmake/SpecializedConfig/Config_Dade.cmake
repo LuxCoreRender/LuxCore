bcd4b6945b19b2e7931e1ebeecc9a868c8295005
David Bucciarelli <dade916@gmail.com>
2020-04-06 19:05:28 +0200

    Updated all Linux libraries

--- cmake/SpecializedConfig/Config_Dade.cmake
+++ cmake/SpecializedConfig/Config_Dade.cmake
@@ -4,9 +4,15 @@
 #
 ###########################################################################
 
-# To compile c-blosc with -fPIC on linux: add "set(CMAKE_POSITION_INDEPENDENT_CODE ON)" to CMakeLists.txt
+# To compile c-blosc:
+# mkdir build
+# cd build
+# cmake -DCMAKE_POSITION_INDEPENDENT_CODE=1 -DPREFER_EXTERNAL_ZLIB=1 -DDEACTIVATE_AVX2=1 -DZLIB_ROOT=/home/david/projects/luxcorerender/LinuxCompile/target-64-sse2 -DCMAKE_INSTALL_PREFIX=../../c-blosc-1.17.1-bin ..
+# cmake --build .
+# cmake --build . --target install
 
-#cmake -DLUXRAYS_CUSTOM_CONFIG=cmake/SpecializedConfig/Config_Dade.cmake -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.7m.so -DPYTHON_INCLUDE_DIR=/usr/include/python3.7m .
+# For LuxCore:
+# cmake -DLUXRAYS_CUSTOM_CONFIG=cmake/SpecializedConfig/Config_Dade.cmake -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.7m.so -DPYTHON_INCLUDE_DIR=/usr/include/python3.7m .
 
 MESSAGE(STATUS "Using Dade's Linux Configuration settings")
 
