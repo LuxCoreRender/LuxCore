9bbe104f9e330bcfbc351f754d78a674e74292e7
u3dreal <48415446+u3dreal@users.noreply.github.com>
2020-04-08 09:40:55 +0200

    possible fix for issue 372
    https://github.com/LuxCoreRender/BlendLuxCore/issues/372

--- scripts/azurepipeline/macos/pack.sh
+++ scripts/azurepipeline/macos/pack.sh
@@ -27,9 +27,9 @@ install_name_tool -id @executable_path/../Resources/libs/libomp.dylib LuxCore.ap
 
 cp -f $DEPS_SOURCE/lib/libembree3.3.dylib LuxCore.app/Contents/Resources/libs/libembree3.3.dylib
 chmod +w LuxCore.app/Contents/Resources/libs/libembree3.3.dylib
-install_name_tool -id @executable_path/../Resources/libs/libembree3.3.dylib LuxCore.app/Contents/Resources/libs/libembree3.3.dylib
-install_name_tool -change @rpath/libtbb.dylib @executable_path/../Resources/libs/libtbb.dylib LuxCore.app/Contents/Resources/libs/libembree3.3.dylib
-install_name_tool -change @rpath/libtbbmalloc.dylib @executable_path/../Resources/libs/libtbbmalloc.dylib LuxCore.app/Contents/Resources/libs/libembree3.3.dylib
+#install_name_tool -id @executable_path/../Resources/libs/libembree3.3.dylib LuxCore.app/Contents/Resources/libs/libembree3.3.dylib
+#install_name_tool -change @rpath/libtbb.dylib @executable_path/../Resources/libs/libtbb.dylib LuxCore.app/Contents/Resources/libs/libembree3.3.dylib
+#install_name_tool -change @rpath/libtbbmalloc.dylib @executable_path/../Resources/libs/libtbbmalloc.dylib LuxCore.app/Contents/Resources/libs/libembree3.3.dylib
 
 #libOpenImageDenoise
 
@@ -111,9 +111,9 @@ install_name_tool -id @loader_path/libomp.dylib ./libomp.dylib
 
 cp -f $DEPS_SOURCE/lib/libembree3.3.dylib ./libembree3.3.dylib
 chmod +w ./libembree3.3.dylib
-install_name_tool -id @loader_path/libembree3.3.dylib ./libembree3.3.dylib
-install_name_tool -change @rpath/libtbb.dylib @loader_path/libtbb.dylib ./libembree3.3.dylib
-install_name_tool -change @rpath/libtbbmalloc.dylib @loader_path/libtbbmalloc.dylib ./libembree3.3.dylib
+#install_name_tool -id @loader_path/libembree3.3.dylib ./libembree3.3.dylib
+#install_name_tool -change @rpath/libtbb.dylib @loader_path/libtbb.dylib ./libembree3.3.dylib
+#install_name_tool -change @rpath/libtbbmalloc.dylib @loader_path/libtbbmalloc.dylib ./libembree3.3.dylib
 
 #libtbb
 
