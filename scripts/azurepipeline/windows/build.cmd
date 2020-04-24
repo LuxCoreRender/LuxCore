aff9190d6b0bc01293e412a60239e294d6725aa0
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2020-04-24 09:36:37 +0200

    Removed copying of Cuda toolkit to WindowsCompileDeps

--- scripts/azurepipeline/windows/build.cmd
+++ scripts/azurepipeline/windows/build.cmd
@@ -1,7 +1,7 @@
 cd ..\WindowsCompile
 set OCL_ROOT=%CD%\OCL_SDK_Light
-::set CUDA_PATH="%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.1"
-::set CUDA_PATH_V10_1="%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.1"
+set CUDA_PATH="%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.1"
+set CUDA_PATH_V10_1="%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.1"
 call .\cmake-build-x64.bat %1 %2 %3 %4
 if ERRORLEVEL 1 exit /B 1
 cd ..
