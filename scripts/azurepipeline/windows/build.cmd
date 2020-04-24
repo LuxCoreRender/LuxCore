cdd46543cce48dbad987bee4d2ce62c2ba8e6b1b
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2020-04-24 15:35:43 +0200

    More tentative fix

--- scripts/azurepipeline/windows/build.cmd
+++ scripts/azurepipeline/windows/build.cmd
@@ -1,5 +1,6 @@
 cd ..\WindowsCompile
 set OCL_ROOT=%CD%\OCL_SDK_Light
+set
 ::set CUDA_PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.1
 ::set CUDA_PATH_V10_1=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.1
 call .\cmake-build-x64.bat %1 %2 %3 %4
