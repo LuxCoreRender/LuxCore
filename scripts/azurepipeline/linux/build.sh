4abfb9358283b0ed072f72ae237dd5076e7dc92c
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2020-04-30 18:05:21 +0200

    Install the full CUDA toolkit including drivers for Linux builds

--- scripts/azurepipeline/linux/build.sh
+++ scripts/azurepipeline/linux/build.sh
@@ -9,7 +9,7 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/
 sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo apt-get -q update
-sudo apt-get install -y cuda-toolkit-10-1
+sudo apt-get install -y cuda-10-1
 export PATH=/usr/local/cuda-10.1/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
