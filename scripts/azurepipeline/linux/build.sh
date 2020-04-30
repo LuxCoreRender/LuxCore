2fbb8ecfd296fac038b056031d16e626a0092675
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2020-04-30 11:52:10 +0200

    Another try

--- scripts/azurepipeline/linux/build.sh
+++ scripts/azurepipeline/linux/build.sh
@@ -4,7 +4,8 @@
 sudo apt-get -qq update
 sudo apt-get install -y libtool-bin cmake flex bison libgtk-3-dev libgl1-mesa-dev python3-numpy ocl-icd-opencl-dev
 # Install CUDA (https://docs.nvidia.com/cuda/archive/10.1/cuda-installation-guide-linux/index.html)
-sudo dpkg -i https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo apt-get -q update
 sudo apt-get install -y cuda-nvcc cuda-cudart cuda-cudart-dev cuda-nvrtc cuda-nvrtc-dev
