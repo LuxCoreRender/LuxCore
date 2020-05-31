b3c0ae92d0b5b3d388cdc9aa726122bed14787db
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2020-05-31 15:51:10 +0200

    Updated Linux Azure Pipeline files for unified CPU/OpenCL/CUDA binaries

--- scripts/azurepipeline/linux/build.sh
+++ scripts/azurepipeline/linux/build.sh
@@ -35,7 +35,7 @@ fi
 cd LinuxCompile
 
 #==========================================================================
-# Compiling OpenCL-less version"
+# Compiling unified CPU/OpenCL/CUDA version"
 #==========================================================================
 
 # Clone LuxCore (this is a bit a waste but LinuxCompile procedure
@@ -44,27 +44,5 @@ git clone .. LuxCore$SDK_BUILD
 ./build-64-sse2 LuxCore$SDK_BUILD
 cp target-64-sse2/LuxCore$SDK_BUILD.tar.bz2 target-64-sse2/luxcorerender-$VERSION_STRING-linux64$SDK_BUILD.tar.bz2
 mv target-64-sse2/LuxCore$SDK_BUILD.tar.bz2 $BUILD_ARTIFACTSTAGINGDIRECTORY/luxcorerender-$VERSION_STRING-linux64$SDK_BUILD.tar.bz2
-
-#==========================================================================
-# Compiling OpenCL version"
-#==========================================================================
-
-# Clone LuxCore (this is a bit a waste but LinuxCompile procedure
-# doesn't work with symbolic links)
-git clone .. LuxCore-opencl$SDK_BUILD
-./build-64-sse2 LuxCore-opencl$SDK_BUILD 5
-cp target-64-sse2/LuxCore-opencl$SDK_BUILD.tar.bz2 target-64-sse2/luxcorerender-$VERSION_STRING-linux64-opencl$SDK_BUILD.tar.bz2
-mv target-64-sse2/LuxCore-opencl$SDK_BUILD.tar.bz2 $BUILD_ARTIFACTSTAGINGDIRECTORY/luxcorerender-$VERSION_STRING-linux64-opencl$SDK_BUILD.tar.bz2
-
-#==========================================================================
-# Compiling CUDA version"
-#==========================================================================
-
-# Clone LuxCore (this is a bit a waste but LinuxCompile procedure
-# doesn't work with symbolic links)
-git clone .. LuxCore-cuda$SDK_BUILD
-./build-64-sse2 LuxCore-cuda$SDK_BUILD 5
-cp target-64-sse2/LuxCore-cuda$SDK_BUILD.tar.bz2 target-64-sse2/luxcorerender-$VERSION_STRING-linux64-cuda$SDK_BUILD.tar.bz2
-mv target-64-sse2/LuxCore-cuda$SDK_BUILD.tar.bz2 $BUILD_ARTIFACTSTAGINGDIRECTORY/luxcorerender-$VERSION_STRING-linux64-cuda$SDK_BUILD.tar.bz2
 
 cd ..
