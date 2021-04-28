9163f051efd2313a5a7d40e470e79c33a304b7f5
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2021-04-28 19:49:12 +0200

    Restoring CUDA Toolkit in Windows build pipeline

--- scripts/azurepipeline/windows/deps-setup.cmd
+++ scripts/azurepipeline/windows/deps-setup.cmd
@@ -6,6 +6,14 @@ git clone --branch master https://github.com/LuxCoreRender/WindowsCompile .\Wind
 
 mklink /J Luxcore %SYSTEM_DEFAULTWORKINGDIRECTORY%
 
+.\WindowsCompile\support\bin\wget.exe https://developer.download.nvidia.com/compute/cuda/10.1/Prod/network_installers/cuda_10.1.243_win10_network.exe
+call .\cuda_10.1.243_win10_network.exe -s nvcc_10.1 cudart_10.1 nvrtc_10.1 nvrtc_dev_10.1 visual_studio_integration_10.1
+
+set CUDA_PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.1
+set CUDA_PATH_V10_1=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.1
+echo "##vso[task.setvariable variable=cuda.path]%CUDA_PATH%"
+echo "##vso[task.setvariable variable=cuda.path.v10.1]%CUDA_PATH_V10_1%"
+
 pip install --upgrade pip
 
 pip install --upgrade setuptools
