00366fbbe7e306a41985cb466f0534379bbae61f
David Bucciarelli <dade916@gmail.com>
2021-10-31 11:25:00 +0100

    Fixed a problem with indirect light going trough objects with white shadow transparency in BIDIRCPU

--- src/slg/engines/bidircpu/bidircputhread.cpp
+++ src/slg/engines/bidircpu/bidircputhread.cpp
@@ -356,7 +356,7 @@ void BiDirCPURenderThread::ConnectToEye(const float time,
 			BSDF bsdfConn;
 			Spectrum connectionThroughput;
 			PathVolumeInfo volInfo = lightVertex.volInfo; // I need to use a copy here
-			if (!scene->Intersect(device, LIGHT_RAY | CAMERA_RAY, &volInfo, u0, &traceRay, &traceRayHit, &bsdfConn,
+			if (!scene->Intersect(device, LIGHT_RAY | CAMERA_RAY | SHADOW_RAY, &volInfo, u0, &traceRay, &traceRayHit, &bsdfConn,
 					&connectionThroughput)) {
 				// Nothing was hit, the light path vertex is visible
 
